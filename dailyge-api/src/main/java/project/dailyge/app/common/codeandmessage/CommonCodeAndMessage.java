611bf3dc46d132e9efcdcb7caf7d86ef7ad19b44
devjun10 <devjun1023@gmail.com>
2024-05-26 02:36:30 +0900

    refactor: 공통 CodeAndMessage 추가 - UnAuthorized(403)

--- dailyge-api/src/main/java/project/dailyge/app/common/codeandmessage/CommonCodeAndMessage.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/codeandmessage/CommonCodeAndMessage.java
@@ -2,7 +2,9 @@ package project.dailyge.app.common.codeandmessage;
 
 public enum CommonCodeAndMessage implements CodeAndMessage {
     OK(200, "OK"),
-    CREATED(201, "Created");
+    CREATED(201, "Created"),
+    INVALID_USER_ID(403, "올바른 사용자 아이디가 아닙니다."),
+    UN_AUTHORIZED(403, "권한이 존재하지 않습니다.");
 
     private final int code;
     private final String message;

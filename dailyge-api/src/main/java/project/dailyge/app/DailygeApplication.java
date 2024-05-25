f8f5a5cbf94013ca44ff902493c538266aa35a7d
devjun10 <devjun1023@gmail.com>
2024-05-26 01:47:27 +0900

    feat: Jpa Entity/Repository 스캔 경로 추가 - 멀티 모듈에서 Jpa Entity/Repository를 식별하기 위한 경로 설정 추가

--- dailyge-api/src/main/java/project/dailyge/app/DailygeApplication.java
+++ dailyge-api/src/main/java/project/dailyge/app/DailygeApplication.java
@@ -2,8 +2,12 @@ package project.dailyge.app;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.domain.*;
+import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"project.dailyge.domain"})
+@EnableJpaRepositories(basePackages = {"project.dailyge.domain"})
 public class DailygeApplication {
 
     public static void main(String[] args) {

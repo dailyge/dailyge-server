c1a1af8a92104e9f087cf5fa6a7ea8e24d87380f
devjun10 <devjun1023@gmail.com>
2024-05-26 02:36:39 +0900

    refactor: BaseEntity @Getter 추가

--- storage/rdb/src/main/java/project/dailyge/domain/BaseEntity.java
+++ storage/rdb/src/main/java/project/dailyge/domain/BaseEntity.java
@@ -1,23 +1,25 @@
 package project.dailyge.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.*;
 import org.springframework.data.jpa.repository.config.*;
 
 import java.time.*;
 
+@Getter
 @MappedSuperclass
 @EnableJpaAuditing
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "created at")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "last modified at")
+    @Column(name = "last_modified_at")
     private LocalDateTime lastModifiedAt;
 
-    @Column(name = "delete")
-    private Boolean deleted;
+    @Column(name = "deleted")
+    private Boolean deleted = false;
 }

bb8db070f7ce5092407bb3af48c7885ef49a4550
devjun10 <devjun1023@gmail.com>
2024-05-26 18:59:36 +0900

    test: ArgumentResolver 단위 테스트 작성

---
+++ dailyge-api/src/test/java/project/dailyge/app/test/common/AuthArgumentResolverTest.java
@@ -0,0 +1,83 @@
+package project.dailyge.app.test.common;
+
+import jakarta.servlet.http.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import static org.mockito.Mockito.*;
+import org.springframework.web.context.request.*;
+import project.dailyge.app.common.auth.*;
+import project.dailyge.app.common.configuration.web.*;
+import project.dailyge.app.common.exception.*;
+import project.dailyge.app.core.user.application.*;
+import project.dailyge.domain.user.*;
+
+@DisplayName("[UnitTest] AuthArgumentResolver 검증 단위 테스트")
+class AuthArgumentResolverTest {
+
+    private AuthArgumentResolver resolver;
+    private UserReadUseCase userReadUseCase;
+    private NativeWebRequest webRequest;
+    private HttpServletRequest request;
+
+    @BeforeEach
+    void setUp() {
+        userReadUseCase = mock(UserReadUseCase.class);
+        resolver = new AuthArgumentResolver(userReadUseCase);
+        request = mock(HttpServletRequest.class);
+        webRequest = mock(NativeWebRequest.class);
+        when(webRequest.getNativeRequest())
+            .thenReturn(request);
+    }
+
+    @Test
+    @DisplayName("사용자 ID가 존재하고, 올바르다면 인증 객체가 생성된다.")
+    void shouldBeNotNullWhenUserIdIsValid() {
+        User user = new User(1L);
+        when(request.getHeader("dailyge_user_id"))
+            .thenReturn(user.getId().toString());
+        when(userReadUseCase.findById(1L))
+            .thenReturn(user);
+
+        DailygeUser result = (DailygeUser) resolver.resolveArgument(null, null, webRequest, null);
+
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("사용자 ID가 null이면 UnAuthorizedException이 발생한다.")
+    void shouldThrowUnAuthorizedExceptionWhenUserIdIsNull() {
+        when(request.getHeader("dailyge_user_id"))
+            .thenReturn(null);
+
+        assertThatThrownBy(() -> resolver.resolveArgument(null, null, webRequest, null))
+            .isExactlyInstanceOf(UnAuthorizedException.class)
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 ID가 올바르지 않은 타입이면 UnAuthorizedException이 발생한다.")
+    void shouldThrowUnAuthorizedExceptionWhenUserIdIsInvalidType() {
+        when(request.getHeader("dailyge_user_id"))
+            .thenReturn("abc");
+
+        assertThatThrownBy(() -> resolver.resolveArgument(null, null, webRequest, null))
+            .isExactlyInstanceOf(UnAuthorizedException.class)
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 ID가 유효하면 예외가 발생하지 않는다.")
+    void shouldNotThrowExceptionWhenUserIdIsValid() {
+        String validUserId = "456";
+        User expectedUser = new User(456L);
+        when(request.getHeader("dailyge_user_id"))
+            .thenReturn(validUserId);
+        when(userReadUseCase.findById(Long.parseLong(validUserId)))
+            .thenReturn(expectedUser);
+
+        assertDoesNotThrow(
+            () -> ((DailygeUser) resolver.resolveArgument(null, null, webRequest, null))
+        );
+    }
+}

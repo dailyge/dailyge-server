27a8a75b1717b6220f606acb5cc59fd21562c723
devjun10 <devjun1023@gmail.com>
2024-05-28 06:19:35 +0900

    test: SnippetUtils 분리 - 제약조건을 위한 Utils 클래스 생성

--- dailyge-api/src/test/java/project/dailyge/app/test/task/documentationtest/snippet/TaskSnippet.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/task/documentationtest/snippet/TaskSnippet.java
@@ -2,17 +2,13 @@ package project.dailyge.app.test.task.documentationtest.snippet;
 
 import static javax.xml.xpath.XPathEvaluationResult.XPathResultType.NUMBER;
 import static javax.xml.xpath.XPathEvaluationResult.XPathResultType.STRING;
-import org.springframework.restdocs.constraints.ConstraintDescriptions;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import org.springframework.restdocs.payload.RequestFieldsSnippet;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
-import org.springframework.restdocs.snippet.Attributes;
-import project.dailyge.app.core.task.dto.requesst.TaskRegisterRequest;
-
-import java.util.List;
+import static project.dailyge.app.common.SnippetUtils.getAttribute;
 
 public interface TaskSnippet {
     FieldDescriptor[] TASK_CREATE_REQUEST_FIELDS = {
@@ -32,21 +28,4 @@ public interface TaskSnippet {
 
     RequestFieldsSnippet TASK_CREATE_REQUEST_SNIPPET = requestFields(TASK_CREATE_REQUEST_FIELDS);
     ResponseFieldsSnippet TASK_CREATE_RESPONSE_SNIPPET = responseFields(TASK_CREATE_RESPONSE);
-
-    static Attributes.Attribute getAttribute(String field) {
-        StringBuilder sb = new StringBuilder();
-        ConstraintDescriptions constraints = new ConstraintDescriptions(TaskRegisterRequest.class);
-
-        List<String> fieldConstraints = constraints.descriptionsForProperty(field);
-        for (String constraint : fieldConstraints) {
-            sb.append("-")
-                .append(" ")
-                .append(constraint)
-                .append(".")
-                .append("\n")
-                .append("\n");
-        }
-        return Attributes.key("constraints")
-            .value(sb.toString());
-    }
 }

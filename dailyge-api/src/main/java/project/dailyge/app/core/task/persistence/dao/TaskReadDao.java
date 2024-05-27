d5b057becce04ee0b9371d7cac97d6a6be4de576
devjun10 <devjun1023@gmail.com>
2024-05-28 05:51:39 +0900

    refactor: QEntity 재생성

--- dailyge-api/src/main/java/project/dailyge/app/core/task/persistence/dao/TaskReadDao.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/task/persistence/dao/TaskReadDao.java
@@ -1,13 +1,13 @@
 package project.dailyge.app.core.task.persistence.dao;
 
-import com.querydsl.jpa.impl.*;
-import lombok.*;
-import org.springframework.stereotype.*;
-import project.dailyge.app.core.task.persistence.*;
-import project.dailyge.domain.task.*;
-import static project.dailyge.domain.task.QTask.task;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import project.dailyge.app.core.task.persistence.TaskEntityReadRepository;
+import static project.dailyge.domain.task.QTaskJpaEntity.taskJpaEntity;
+import project.dailyge.domain.task.TaskJpaEntity;
 
-import java.util.*;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,8 +18,8 @@ public class TaskReadDao implements TaskEntityReadRepository {
     @Override
     public Optional<TaskJpaEntity> findById(Long taskId) {
         return Optional.ofNullable(
-            queryFactory.selectFrom(task)
-                .where(task.id.eq(taskId))
+            queryFactory.selectFrom(taskJpaEntity)
+                .where(taskJpaEntity.id.eq(taskId))
                 .fetchFirst()
         );
     }

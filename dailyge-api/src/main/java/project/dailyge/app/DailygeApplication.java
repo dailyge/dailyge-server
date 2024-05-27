d06b44d781f8032abd5d2e2eb653cc14da06abd9
devjun10 <devjun1023@gmail.com>
2024-05-28 06:39:55 +0900

    refactor: Application 클래스 Import 변경

--- dailyge-api/src/main/java/project/dailyge/app/DailygeApplication.java
+++ dailyge-api/src/main/java/project/dailyge/app/DailygeApplication.java
@@ -1,9 +1,9 @@
 package project.dailyge.app;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.autoconfigure.domain.*;
-import org.springframework.data.jpa.repository.config.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"project.dailyge.domain"})

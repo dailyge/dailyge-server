8304eb51375d7e34d6a266eda5602161721b0809
devjun10 <devjun1023@gmail.com>
2024-05-28 08:10:05 +0900

    chore: created_by 칼럼 추가

--- dailyge-api/src/main/resources/schema/schema.sql
+++ dailyge-api/src/main/resources/schema/schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS tasks
     date             DATE                                 NOT NULL COMMENT '날짜',
     status           ENUM ('TODO', 'IN_PROGRESS', 'DONE') NOT NULL COMMENT '작업 상태',
     created_at       DATETIME(6)                          NOT NULL COMMENT '생성일',
+    created_by       BIGINT                               NULL COMMENT '생성한 사람',
     last_modified_at DATETIME(6)                          NULL COMMENT '최종 수정일',
     last_modified_by BIGINT                               NULL COMMENT '최종 수정한 사람',
     deleted          BIT                                  NOT NULL COMMENT '삭제 유무'

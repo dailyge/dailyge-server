50c37fa43dbd831a5258b539443f13882ca1c9ac
beatmejy <beatmejy@naver.com>
2024-06-01 16:08:51 +0900

    refactor: 이메일 중복 상태코드 422로 변경.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/exception/UserCodeAndMessage.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/exception/UserCodeAndMessage.java
@@ -5,7 +5,7 @@ import project.dailyge.app.common.codeandmessage.CodeAndMessage;
 public enum UserCodeAndMessage implements CodeAndMessage {
 
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
-    USER_EMAIL_CONFLICT(409, "이미 존재하는 이메일 계정입니다");
+    USER_EMAIL_CONFLICT(422, "이미 존재하는 이메일 계정입니다");
 
     private final int code;
     private final String message;

4cb4880ad0eeea0c03487fe55db22db4e90a5794
beatmejy <beatmejy@naver.com>
2024-06-05 20:53:02 +0900

    test: 실제 유저 저장은 생성자에 PK 안넣도록 수정.

--- dailyge-api/src/test/java/project/dailyge/app/test/task/integrationtest/TaskSaveIntegrationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/task/integrationtest/TaskSaveIntegrationTest.java
@@ -25,7 +25,7 @@ public class TaskSaveIntegrationTest extends IntegrationTestBase {
     @Test
     @DisplayName("할 일이 저장되면, Id가 Null이 아니다.")
     void whenSaveTaskThenTaskIdShouldNotBeNull() {
-        final UserJpaEntity newUser = userWriteUseCase.save(UserFixture.createUserJpaEntity(1L));
+        final UserJpaEntity newUser = userWriteUseCase.save(UserFixture.createUserJpaEntity());
         final TaskJpaEntity newTask = new TaskJpaEntity("독서", "Kafka 완벽가이드 1~30p 읽기", now(), TODO, newUser.getId());
 
         final TaskJpaEntity savedTask = taskFacade.save(newTask);

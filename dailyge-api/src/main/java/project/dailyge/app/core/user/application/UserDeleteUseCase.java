5f7d43ab01fe1373600bcb6c93146e11577d155f
beatmejy <beatmejy@naver.com>
2024-06-06 14:56:11 +0900

    [Revert] 사용자 삭제 구현.
    This reverts commit f0866d34910db02d25b789afaa2f64e233c85860.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/application/UserDeleteUseCase.java
+++
@@ -1,8 +0,0 @@
-package project.dailyge.app.core.user.application;
-
-import project.dailyge.domain.user.UserJpaEntity;
-
-public interface UserDeleteUseCase {
-
-    void delete(UserJpaEntity user);
-}
--- dailyge-api/src/main/java/project/dailyge/app/core/user/application/facade/UserFacade.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/application/facade/UserFacade.java
@@ -3,7 +3,6 @@ package project.dailyge.app.core.user.application.facade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import project.dailyge.app.core.user.application.UserDeleteUseCase;
 import project.dailyge.app.core.user.application.UserReadUseCase;
 import project.dailyge.app.core.user.application.service.UserWriteService;
 import project.dailyge.app.core.user.exception.UserTypeException;
@@ -19,20 +18,13 @@ public class UserFacade {
 
     private final UserReadUseCase userReadUseCase;
     private final UserWriteService userWriteService;
-    private final UserDeleteUseCase userDeleteUseCase;
 
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public UserJpaEntity save(final UserJpaEntity userJpaEntity) {
         Optional<UserJpaEntity> findUser = userReadUseCase.findByEmail(userJpaEntity.getEmail());
         if (findUser.isPresent()) {
             throw UserTypeException.from(USER_EMAIL_CONFLICT);
         }
         return userWriteService.save(userJpaEntity);
-    }
-
-    @Transactional
-    public void delete(Long userId) {
-        UserJpaEntity findUser = userReadUseCase.findById(userId);
-        userDeleteUseCase.delete(findUser);
     }
 }

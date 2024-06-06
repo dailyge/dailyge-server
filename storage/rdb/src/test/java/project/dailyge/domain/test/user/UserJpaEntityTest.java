3e189bda026a54d9c0836685e6120b347df5e205
beatmejy <beatmejy@naver.com>
2024-06-06 20:15:04 +0900

    test: 코드 리팩토링 및 equals, hashcode 단위 테스트 작성.

--- storage/rdb/src/test/java/project/dailyge/domain/test/user/UserJpaEntityTest.java
+++ storage/rdb/src/test/java/project/dailyge/domain/test/user/UserJpaEntityTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import project.dailyge.domain.user.UserJpaEntity;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static project.dailyge.domain.user.UserJpaEntity.*;
 
 @DisplayName("[UnitTest] 유저 엔티티 테스트")
 class UserJpaEntityTest {
@@ -16,22 +18,12 @@ class UserJpaEntityTest {
 	private static final String PROFILE_IMAGE_URL = "profileImgUrl.jpg";
 
 	@Test
-	@DisplayName("사용자 생성하는데 성공한다.")
-	void userCreateSuccessTest() {
-		final UserJpaEntity user = new UserJpaEntity(NICKNAME, EMAIL);
-
-		assertAll(
-			() -> assertEquals(NICKNAME, user.getNickname()),
-			() -> assertEquals(EMAIL, user.getEmail())
-		);
-	}
-
-	@Test
-	@DisplayName("프로필 이미지를 포함하여 생성하는데 성공한다.")
-	void userProfileImageUrlCreateSuccessTest() {
+	@DisplayName("사용자 생성 시, 정상적으로 사용자 인스턴스가 생성된다.")
+	void whenUserCreateThenUserShouldBeNotNull() {
 		final UserJpaEntity user = new UserJpaEntity(NICKNAME, EMAIL, PROFILE_IMAGE_URL);
 
 		assertAll(
+			() -> assertNotNull(user),
 			() -> assertEquals(NICKNAME, user.getNickname()),
 			() -> assertEquals(EMAIL, user.getEmail()),
 			() -> assertEquals(PROFILE_IMAGE_URL, user.getProfileImageUrl())
@@ -39,34 +31,80 @@ class UserJpaEntityTest {
 	}
 
 	@Test
-	@DisplayName("허용 가능한 닉네임 길이를 초과하면 IllegalArgumentException이 발생한다.")
-	void whenMaxLengthOverNicknameThenIllegalArgumentException() {
+	@DisplayName("사용자 생성 시 프로필 이미지를 넣지 않아도, 정상적으로 사용자 인스턴스가 생성된다.")
+	void whenUserCreateByEmptyImageUrlThenUserShouldBeNotNull() {
+		final UserJpaEntity user = new UserJpaEntity(NICKNAME, EMAIL);
+
+		assertAll(
+			() -> assertEquals(NICKNAME, user.getNickname()),
+			() -> assertEquals(EMAIL, user.getEmail()),
+			() -> assertNull(user.getProfileImageUrl())
+		);
+	}
+
+	@Test
+	@DisplayName("허용 가능한 닉네임 길이를 초과하면, IllegalArgumentException이 발생한다.")
+	void whenMaxLengthOverNicknameThenIllegalArgumentExceptionShouldBeHappen() {
 		final String overLengthNickname = "n".repeat(21);
 
-		assertThrowsExactly(IllegalArgumentException.class, () -> new UserJpaEntity(overLengthNickname, EMAIL));
+		assertThatThrownBy(() -> new UserJpaEntity(overLengthNickname, EMAIL))
+			.isExactlyInstanceOf(IllegalArgumentException.class)
+			.hasMessage(getOverMaxNicknameLengthErrorMessage());
 	}
 
 	@Test
 	@DisplayName("허용 가능한 이메일 길이를 초과하면 IllegalArgumentException이 발생한다.")
-	void whenMaxLengthOverEmailThenIllegalArgumentException() {
+	void whenMaxLengthOverEmailThenIllegalArgumentExceptionShouldBeHappen() {
 		final String overLengthEmail = "e".repeat(50) + "@domain.com";
 
-		assertThrowsExactly(IllegalArgumentException.class, () -> new UserJpaEntity(NICKNAME, overLengthEmail));
+		assertThatThrownBy(() -> new UserJpaEntity(NICKNAME, overLengthEmail))
+			.isExactlyInstanceOf(IllegalArgumentException.class)
+			.hasMessage(getOverMaxEmailLengthErrorMessage());
 	}
 
 	@ParameterizedTest
 	@DisplayName("이메일 규격에 맞지 않을 경우 IllegalArgumentException이 발생한다.")
 	@ValueSource(strings = {"email.domain.com", "email@damain,com", "email@domain@domain.com"})
-	void whenNotCompliantEmailThenIllegalArgumentException(String notCompliantEmail){
-		assertThrowsExactly(IllegalArgumentException.class, () -> new UserJpaEntity(NICKNAME, notCompliantEmail));
+	void whenNotCompliantEmailThenIllegalArgumentExceptionShouldBeHappen(String notCompliantEmail){
+		assertThatThrownBy(() -> new UserJpaEntity(NICKNAME, notCompliantEmail))
+			.isExactlyInstanceOf(IllegalArgumentException.class)
+			.hasMessage(UserJpaEntity.getInvalidEmailErrorMessage());
 	}
 
 	@Test
 	@DisplayName("허용 가능한 프로필 이미지 URL 길이를 초과하면 IllegalArgumentException이 발생한다.")
-	void whenMaxLengthOverProfileImageUrlThenIllegalArgumentException() {
+	void whenMaxLengthOverProfileImageUrlThenIllegalArgumentExceptionShouldBeHappen() {
 		final String overLengthProfileImageUrl = "p".repeat(2001);
 
-		assertThrowsExactly(IllegalArgumentException.class, () -> new UserJpaEntity(NICKNAME, EMAIL, overLengthProfileImageUrl));
+		assertThatThrownBy(() -> new UserJpaEntity(NICKNAME, EMAIL, overLengthProfileImageUrl))
+			.isExactlyInstanceOf(IllegalArgumentException.class)
+			.hasMessage(getOverMaxProfileImageUrlErrorMessage());
 	}
 
+	@Test
+	@DisplayName("사용자 ID가 같다면, 동일한 객체이다.")
+	void whenUserIdSameThenInstanceShouldBeSame() {
+		UserJpaEntity user1 = new UserJpaEntity(1L, "test", "test@gmail.com");
+		UserJpaEntity user2 = new UserJpaEntity(1L, "test", "test@gmail.com");
+
+		assertEquals(user1, user2);
+	}
+
+	@Test
+	@DisplayName("사용자 ID가 다르다면, 다른 객체이다.")
+	void whenUserIdDifferentThenInstanceShouldBeDifferent() {
+		UserJpaEntity user1 = new UserJpaEntity(1L, "test", "test@gmail.com");
+		UserJpaEntity user2 = new UserJpaEntity(2L, "test", "test@gmail.com");
+
+		assertNotEquals(user1, user2);
+	}
+
+	@Test
+	@DisplayName("사용자 ID가 같다면, 해시코드가 동일하다.")
+	void whenUserIdSameThenHashcodeShouldBeSame() {
+		UserJpaEntity user1 = new UserJpaEntity(1L, "test", "test@gmail.com");
+		UserJpaEntity user2 = new UserJpaEntity(1L, "test", "test@gmail.com");
+
+		assertEquals(user1.hashCode(), user2.hashCode());
+	}
 }

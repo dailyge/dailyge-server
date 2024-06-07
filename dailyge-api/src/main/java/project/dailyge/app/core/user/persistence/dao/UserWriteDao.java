a8f2f5ad3dc34336d78e02c2e8d655e38b198951
beatmejy <beatmejy@naver.com>
2024-06-07 18:12:20 +0900

    test: DAO Transactional 삭제.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/persistence/dao/UserWriteDao.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/persistence/dao/UserWriteDao.java
@@ -3,7 +3,6 @@ package project.dailyge.app.core.user.persistence.dao;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import project.dailyge.domain.user.UserEntityWriteRepository;
 import project.dailyge.domain.user.UserJpaEntity;
 
@@ -14,7 +13,6 @@ public class UserWriteDao implements UserEntityWriteRepository {
     private final EntityManager entityManager;
 
     @Override
-    @Transactional
     public UserJpaEntity save(UserJpaEntity user) {
         entityManager.persist(user);
         return user;

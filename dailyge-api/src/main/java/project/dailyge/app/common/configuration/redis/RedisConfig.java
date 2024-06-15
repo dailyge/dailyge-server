d0deaa314bc64cc9c081bfda331d5c3e2dc7d723
beatmejy <beatmejy@naver.com>
2024-06-15 22:13:22 +0900

    feat: Redis 설정 및 리프레시 토큰 저장.

---
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/redis/RedisConfig.java
@@ -0,0 +1,33 @@
+package project.dailyge.app.common.configuration.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(){
+        StringRedisTemplate stringRedisTemplate=new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+        return stringRedisTemplate;
+    }
+}
--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/WebConfiguration.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/WebConfiguration.java
@@ -2,8 +2,10 @@ package project.dailyge.app.common.configuration.web;
 
 import lombok.*;
 import org.springframework.context.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.*;
 import org.springframework.web.servlet.config.annotation.*;
+import project.dailyge.app.common.auth.TokenProvider;
 import project.dailyge.app.core.user.application.*;
 
 import java.util.*;
@@ -13,9 +15,15 @@ import java.util.*;
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final UserReadUseCase userReadUseCase;
+    private final TokenProvider tokenProvider;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthArgumentResolver(userReadUseCase));
+        resolvers.add(new AuthArgumentResolver(userReadUseCase, tokenProvider));
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }

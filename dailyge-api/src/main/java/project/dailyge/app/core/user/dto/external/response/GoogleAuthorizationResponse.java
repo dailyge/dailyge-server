2f27f9b4864f5ebb3a4ae77550b8b2ca7220c5ee
beatmejy <beatmejy@naver.com>
2024-06-17 17:11:31 +0900

    chore: 주석 된 필요없는 코드 삭제.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/dto/external/response/GoogleAuthorizationResponse.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/dto/external/response/GoogleAuthorizationResponse.java
@@ -16,9 +16,6 @@ public class GoogleAuthorizationResponse {
     @JsonProperty(value = "expires_in")
     private Long expiresIn;
 
-//    @JsonProperty(value = "refresh_token")
-//    private String refreshToken;
-
     private String scope;
 
     @JsonProperty(value = "token_type")

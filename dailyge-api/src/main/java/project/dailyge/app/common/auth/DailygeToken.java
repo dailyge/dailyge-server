63777fc1b281aae253d766e0c5d5af6a8e7dfdc6
beatmejy <beatmejy@naver.com>
2024-06-18 13:59:52 +0900

    refactor: 코드 재사용성 늘리기.

--- dailyge-api/src/main/java/project/dailyge/app/common/auth/DailygeToken.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/auth/DailygeToken.java
@@ -5,12 +5,18 @@ public record DailygeToken(
     String refreshToken,
     int maxAge
 ) {
+    private static final String BEARER =  "Bearer ";
+
+    public String getAuthorizationToken() {
+        return BEARER + accessToken;
+    }
+
     public String getRefreshTokenCookie() {
         StringBuilder builder = new StringBuilder();
-        builder.append("refreshToken=" + refreshToken + ";");
+        builder.append("refreshToken=").append(refreshToken).append(";");
         builder.append("HttpOnly=true;");
         builder.append("Secure=true;");
-        builder.append("Max-Age=" + maxAge + ";");
+        builder.append("Max-Age=").append(maxAge).append(";");
         return builder.toString();
     }
 

311769cfc3d5ea4c6863b9dc2505dd6d9e09a85d
beatmejy <beatmejy@naver.com>
2024-06-18 21:02:22 +0900

    refactor: SecurityConfig oauth2Login 삭제.

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/SecurityConfig.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/SecurityConfig.java
@@ -3,11 +3,11 @@ package project.dailyge.app.common.configuration.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -17,8 +17,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/oauth2").permitAll()
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll()
-            )
-            .oauth2Login(withDefaults());
+            );
         return http.build();
     }
 }

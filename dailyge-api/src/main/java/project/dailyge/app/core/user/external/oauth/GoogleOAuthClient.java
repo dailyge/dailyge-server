9c0b0bc9914248924416152e78888f5f6877b332
beatmejy <beatmejy@naver.com>
2024-06-18 20:48:49 +0900

    refactor: 외부 API 통신 try-catch 추가.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/GoogleOAuthClient.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/GoogleOAuthClient.java
@@ -1,6 +1,7 @@
 package project.dailyge.app.core.user.external.oauth;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -12,8 +13,10 @@ import project.dailyge.app.core.user.presentation.GoogleAuthorizationRequest;
 import project.dailyge.app.core.user.presentation.response.external.GoogleAuthorizationResponse;
 import project.dailyge.app.core.user.presentation.response.external.GoogleUserInfoResponse;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpMethod.GET;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class GoogleOAuthClient {
@@ -32,23 +35,36 @@ public class GoogleOAuthClient {
         final ClientRegistration clientRegistration,
         final String code
     ) {
-        GoogleAuthorizationRequest request = new GoogleAuthorizationRequest(code, clientRegistration);
-        GoogleAuthorizationResponse authorizationResponse =
-            restTemplate.postForObject(authorizationUrl, request, GoogleAuthorizationResponse.class);
-
+        final GoogleAuthorizationRequest authorizationRequest = new GoogleAuthorizationRequest(code, clientRegistration);
+        final GoogleAuthorizationResponse authorizationResponse = googleAuthorization(authorizationRequest);
         if (authorizationResponse == null) {
             throw new CommonException(CommonCodeAndMessage.BAD_GATEWAY);
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", BEARER + authorizationResponse.getAccessToken());
-        ResponseEntity<GoogleUserInfoResponse> userInfoResponse =
-            restTemplate.exchange(userAccessUrl, GET, new HttpEntity<>(headers), GoogleUserInfoResponse.class);
-
-        if (!userInfoResponse.getStatusCode().equals(HttpStatus.OK)) {
+        final ResponseEntity<GoogleUserInfoResponse> userInfoResponse = getGoogleUserInfo(authorizationResponse);
+        if (userInfoResponse == null || !userInfoResponse.getStatusCode().equals(HttpStatus.OK)) {
             throw new CommonException(CommonCodeAndMessage.BAD_GATEWAY);
         }
-
         return userInfoResponse.getBody();
+    }
+
+    private GoogleAuthorizationResponse googleAuthorization(GoogleAuthorizationRequest request) {
+        try {
+            return restTemplate.postForObject(authorizationUrl, request, GoogleAuthorizationResponse.class);
+        } catch (Exception e) {
+            log.error("Google OAuth2 권한 인증 예외:{}", e.getMessage());
+        }
+        return null;
+    }
+
+    private ResponseEntity<GoogleUserInfoResponse> getGoogleUserInfo(GoogleAuthorizationResponse authorizationResponse) {
+        try {
+            final HttpHeaders headers = new HttpHeaders();
+            headers.set(AUTHORIZATION, BEARER + authorizationResponse.getAccessToken());
+            return restTemplate.exchange(userAccessUrl, GET, new HttpEntity<>(headers), GoogleUserInfoResponse.class);
+        } catch (Exception e) {
+            log.error("Google OAuth2 사용자 정보 조회 예외:{}", e.getMessage());
+        }
+        return null;
     }
 }

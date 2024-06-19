f178d32ef37ce0a3ec9d31e4d1e40b794bbad8b9
beatmejy <beatmejy@naver.com>
2024-06-19 18:50:34 +0900

    refactor: Security 권한 설정 변경.

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/SecurityConfig.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/SecurityConfig.java
@@ -1,21 +1,25 @@
 package project.dailyge.app.common.configuration.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(FormLoginConfigurer::disable)
             .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                .requestMatchers("/api/oauth2").permitAll()
-                .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll()
             );
         return http.build();

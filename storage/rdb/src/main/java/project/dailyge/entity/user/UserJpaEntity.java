6260ea44771f0dfdad0937567916a31c6094b80e
beatmejy <beatmejy@naver.com>
2024-06-20 14:54:48 +0900

    refactor: 이메일 검증 성능 및 길이 검증 개선

--- storage/rdb/src/main/java/project/dailyge/entity/user/UserJpaEntity.java
+++ storage/rdb/src/main/java/project/dailyge/entity/user/UserJpaEntity.java
@@ -19,7 +19,7 @@ public class UserJpaEntity extends BaseEntity {
     private static final int MAX_NICKNAME_LENGTH = 20;
     private static final int MAX_EMAIL_LENGTH = 50;
     private static final int MAX_PROFILE_IMAGE_URL_LENGTH = 2000;
-    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@gmail\\.com$";
+    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z](?:[-_.]?[0-9a-zA-Z]){0,39}@gmail\\.com$";
     private static final  String OVER_MAX_NICKNAME_LENGTH_ERROR_MESSAGE = "입력 가능한 최대 닉네임 길이를 초과했습니다.";
     private static final String INVALID_EMAIL_ERROR_MESSAGE = "유효하지 않는 이메일 형식입니다.";
     private static final String OVER_MAX_PROFILE_IMAGE_URL_ERROR_MESSAGE = "입력 가능한 최대 URL 길이를 초과했습니다.";

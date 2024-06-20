f580a7c47a0de7c5ef5c372048cf28026a0f5215
beatmejy <beatmejy@naver.com>
2024-06-20 14:23:07 +0900

    style: 불필요한 임포트 제거.

--- storage/rdb/src/main/java/project/dailyge/entity/task/TaskEntityWriteRepository.java
+++ storage/rdb/src/main/java/project/dailyge/entity/task/TaskEntityWriteRepository.java
@@ -1,7 +1,5 @@
 package project.dailyge.entity.task;
 
-import project.dailyge.entity.task.TaskJpaEntity;
-
 public interface TaskEntityWriteRepository {
     TaskJpaEntity save(TaskJpaEntity task);
 }

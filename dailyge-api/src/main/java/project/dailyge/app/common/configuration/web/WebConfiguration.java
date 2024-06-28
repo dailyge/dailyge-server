dacfc7268bf0bd58a6aa1e2d811ea77765a8a81f
beatmejy <beatmejy@naver.com>
2024-06-28 15:43:10 +0900

    feat: 로그인 페이지 요청 시 로그인 상태 검증 interceptor 개발.

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/WebConfiguration.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/WebConfiguration.java
@@ -3,8 +3,10 @@ package project.dailyge.app.common.configuration.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.dailyge.app.common.auth.TokenProvider;
+import project.dailyge.app.common.configuration.interceptor.LoginInterceptor;
 import project.dailyge.app.core.user.application.UserReadUseCase;
 
 import java.util.List;
@@ -15,9 +17,15 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     private final UserReadUseCase userReadUseCase;
     private final TokenProvider tokenProvider;
+    private final LoginInterceptor loginInterceptor;
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthArgumentResolver(userReadUseCase, tokenProvider));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
     }
 }

da1e2279cdb944e3328943c5bb88e611e36bdc3c
beatmejy <beatmejy@naver.com>
2024-07-01 19:33:12 +0900

    feat: 리프레시 토큰으로 재 발급 로직 개발.

--- dailyge-api/src/main/java/project/dailyge/app/core/common/web/LoginInterceptor.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/common/web/LoginInterceptor.java
@@ -1,54 +1,115 @@
 package project.dailyge.app.core.common.web;
 
+import com.nimbusds.jose.shaded.gson.Gson;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import project.dailyge.app.common.auth.DailygeToken;
 import project.dailyge.app.common.auth.TokenProvider;
-import project.dailyge.app.common.codeandmessage.CommonCodeAndMessage;
-import project.dailyge.app.common.exception.UnAuthorizedException;
 import project.dailyge.app.core.user.application.UserReadUseCase;
-import project.dailyge.app.core.user.exception.UserTypeException;
+import project.dailyge.app.core.user.external.oauth.TokenManager;
+import project.dailyge.entity.user.UserJpaEntity;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Optional;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LoginInterceptor implements HandlerInterceptor {
 
-    private static final String LOGIN_URL = "/api/login";
+    private static final String LOGIN_URI = "/api/login";
     private static final String DEFAULT_REFERER = "/";
     private static final String REFERER = "referer";
+    private static final String REFRESH_TOKEN = "Refresh-Token";
+    private static final String ACCESS_TOKEN = "accessToken";
 
     private final UserReadUseCase userReadUseCase;
     private final TokenProvider tokenProvider;
+    private final TokenManager tokenManager;
 
     @Override
     public boolean preHandle(
         final HttpServletRequest request,
         final HttpServletResponse response,
         final Object handler
-    ) throws IOException {
-        if (!LOGIN_URL.equals(request.getRequestURI())) {
-            return true;
-        }
-        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        final String accessToken = tokenProvider.getAccessToken(authorizationHeader);
+    ) {
         try {
+            if (!LOGIN_URI.equals(request.getRequestURI())) {
+                return true;
+            }
+            final String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if (authorizationHeader == null) {
+                return true;
+            }
+            final String accessToken = tokenProvider.getAccessToken(authorizationHeader);
             tokenProvider.validateToken(accessToken);
             final Long userId = tokenProvider.getUserId(accessToken);
             userReadUseCase.findActiveUserById(userId);
-
-            final String referer = request.getHeader(REFERER);
-            response.setStatus(CommonCodeAndMessage.FOUND.code());
-            final PrintWriter writer = response.getWriter();
-            writer.print(referer == null ? DEFAULT_REFERER : referer);
+            setLoggedResponse(request, response, accessToken);
             return false;
-        } catch (UnAuthorizedException | UserTypeException ex) {
+        } catch (ExpiredJwtException ex) {
+            return refreshToken(request, response, ex);
+        } catch (Exception ex) {
+            log.error("abnormal token error: {}", ex);
             return true;
         }
+    }
+
+    private boolean refreshToken(
+        final HttpServletRequest request,
+        final HttpServletResponse response,
+        final ExpiredJwtException expiredJwtException
+    ) {
+        try {
+            Optional<Cookie> cookieStream = Arrays.stream(request.getCookies())
+                .filter(cookie -> REFRESH_TOKEN.equals(cookie.getName()))
+                .findFirst();
+            if (cookieStream.isEmpty()) {
+                return true;
+            }
+            final Cookie cookie = cookieStream.get();
+            final String refreshToken = cookie.getValue();
+            final Claims claims = expiredJwtException.getClaims();
+            final Long userId = claims.get("id", Long.class);
+            userReadUseCase.findActiveUserById(userId);
+            if (!tokenManager.getRefreshTokenKey(userId).equals(refreshToken)) {
+                return true;
+            }
+            final UserJpaEntity user = userReadUseCase.findActiveUserById(userId);
+            final DailygeToken token = tokenProvider.createToken(user);
+            setLoggedResponse(request, response, token.accessToken());
+            return false;
+        } catch (Exception ex) {
+            log.error("refresh token error", ex);
+            return true;
+        }
+    }
+
+    private void setLoggedResponse(
+        final HttpServletRequest request,
+        final HttpServletResponse response,
+        final String accessToken
+    ) throws IOException {
+        final HashMap<String, String> jsonBody = new HashMap<>();
+        final String referer = request.getHeader(REFERER);
+        jsonBody.put("url", referer == null ? DEFAULT_REFERER : referer);
+        jsonBody.put(ACCESS_TOKEN, accessToken);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        final PrintWriter writer = response.getWriter();
+        final Gson gson = new Gson();
+        writer.print(gson.toJson(jsonBody));
     }
 }

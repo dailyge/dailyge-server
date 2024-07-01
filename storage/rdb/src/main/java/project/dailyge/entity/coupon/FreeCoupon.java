e51ee87e78a97e95da95c5316bc4729c7ceb8c28
devjun10 <devjun1023@gmail.com>
2024-07-01 17:27:29 +0900

    feat: Coupon 엔티티 매핑

---
+++ storage/rdb/src/main/java/project/dailyge/entity/coupon/FreeCoupon.java
@@ -0,0 +1,33 @@
+package project.dailyge.entity.coupon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import project.dailyge.entity.BaseEntity;
+
+@Getter
+@Entity(name = "free_coupons")
+public class FreeCoupon extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "serial_number")
+    private String serialNumber;
+
+    @Column(name = "issuer_id")
+    private Long issuerId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "event_id")
+    private Long eventId;
+}

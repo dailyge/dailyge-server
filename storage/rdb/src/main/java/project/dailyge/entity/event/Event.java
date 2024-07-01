23e652d24b84ddc83982b883178ca4720969fa75
devjun10 <devjun1023@gmail.com>
2024-07-01 17:27:41 +0900

    feat: Event 엔티티 매핑

---
+++ storage/rdb/src/main/java/project/dailyge/entity/event/Event.java
@@ -0,0 +1,32 @@
+package project.dailyge.entity.event;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import project.dailyge.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity(name = "events")
+public class Event extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+}

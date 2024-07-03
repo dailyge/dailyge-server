9a8824a3b648e3c9026f04a62beedbfdb0184171
beatmejy <beatmejy@naver.com>
2024-07-03 19:09:02 +0900

    test: 일급 컬렉션(Cookies) 단위테스트 작성.

---
+++ dailyge-api/src/test/java/project/dailyge/app/test/common/CookiesUnitTest.java
@@ -0,0 +1,36 @@
+package project.dailyge.app.test.common;
+
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import project.dailyge.app.core.common.web.Cookies;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@DisplayName("[UnitTest] Cookies 단위 테스트")
+class CookiesUnitTest {
+
+    private static final String KEY = "dailyge";
+    private static final String VALUE = "good";
+
+    @Test
+    @DisplayName("존재하는 쿠키의 값을 꺼내면, 쿠키 값이 반환한다.")
+    void whenExistsCookieThenResultShouldBeExists() {
+        final Cookie[] cookie = new Cookie[1];
+        cookie[0] = new Cookie(KEY, VALUE);
+        final Cookies cookies = new Cookies(cookie);
+
+        assertEquals(VALUE, cookies.getValueByKey(KEY));
+    }
+
+    @Test
+    @DisplayName("존재하지 않은 쿠키의 값을 꺼내면, Null을 반환한다.")
+    void whenThen() {
+        final Cookie[] cookie = new Cookie[1];
+        cookie[0] = new Cookie(KEY, VALUE);
+        final Cookies cookies = new Cookies(cookie);
+
+        assertNull(cookies.getValueByKey("test"));
+    }
+}

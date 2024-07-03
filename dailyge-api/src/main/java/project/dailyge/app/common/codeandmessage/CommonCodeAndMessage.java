07d387f32c4fb2573b43894d7c146989c93deb7a
beatmejy <beatmejy@naver.com>
2024-07-03 18:17:40 +0900

    refactor: 토큰 생성과정 예외 처리 추가.

--- dailyge-api/src/main/java/project/dailyge/app/common/codeandmessage/CommonCodeAndMessage.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/codeandmessage/CommonCodeAndMessage.java
@@ -9,7 +9,7 @@ public enum CommonCodeAndMessage implements CodeAndMessage {
     INVALID_USER_ID(403, "올바른 사용자 아이디가 아닙니다."),
     INVALID_USER_TOKEN(403, "유효하지 않는 사용자 토큰입니다."),
     UN_AUTHORIZED(403, "권한이 존재하지 않습니다."),
-    INTERNAL_SERVER_ERROR(500, "서버 내부 입니다."),
+    INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다."),
     BAD_GATEWAY(502, "외부 서버와 통신하는 과정에서 오류가 발생했습니다.");
 
     private final int code;

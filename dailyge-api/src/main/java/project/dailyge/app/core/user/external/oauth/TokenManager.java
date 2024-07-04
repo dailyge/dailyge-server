d101b7553d82435379f358c2148261cb927c678b
beatmejy <beatmejy@naver.com>
2024-07-04 14:15:43 +0900

    refactor: 레디스 예외 상황별 처리 추가.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/TokenManager.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/TokenManager.java
@@ -1,13 +1,16 @@
 package project.dailyge.app.core.user.external.oauth;
 
+import io.netty.handler.timeout.TimeoutException;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.type.SerializationException;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import project.dailyge.app.common.exception.ExternalServerException;
+import project.dailyge.app.core.user.exception.UserTypeException;
 
-import static project.dailyge.app.common.codeandmessage.CommonCodeAndMessage.BAD_GATEWAY;
-import static project.dailyge.app.common.exception.ExternalServerException.REDIS_SAVE_FAILED_MESSAGE;
-import static project.dailyge.app.common.exception.ExternalServerException.REDIS_SEARCH_FAILED_MESSAGE;
+import static project.dailyge.app.common.codeandmessage.CommonCodeAndMessage.*;
+import static project.dailyge.app.core.user.exception.UserCodeAndMessage.EMPTY_USER_ID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +22,40 @@ public class TokenManager {
         final Long userId,
         final String refreshToken
     ) {
+        if (userId == null) {
+            throw UserTypeException.from(EMPTY_USER_ID);
+        }
         try {
-            redisTemplate.opsForValue().set(String.format("user:refreshToken:%s", userId.toString()), refreshToken);
+            redisTemplate.opsForValue().set(String.format("user:refreshToken:%s", userId), refreshToken);
+        } catch (IllegalArgumentException ex) {
+            throw new ExternalServerException(ex.getMessage(), BAD_REQUEST);
+        } catch (RedisConnectionFailureException ex) {
+            throw new ExternalServerException(ex.getMessage(), SERVICE_UNAVAILABLE);
+        } catch (TimeoutException ex) {
+            throw new ExternalServerException(ex.getMessage(), GATEWAY_TIMEOUT);
+        } catch (SerializationException ex) {
+            throw new ExternalServerException(ex.getMessage(), INTERNAL_SERVER_ERROR);
         } catch (Exception ex) {
-            throw new ExternalServerException(REDIS_SAVE_FAILED_MESSAGE, BAD_GATEWAY);
+            throw new ExternalServerException(ex.getMessage(), BAD_GATEWAY);
         }
     }
 
     public String getRefreshTokenKey(final Long userId) {
+        if (userId == null) {
+            throw UserTypeException.from(EMPTY_USER_ID);
+        }
         try {
-            return redisTemplate.opsForValue().get(String.format("user:refreshToken:%s", userId.toString()));
+            return redisTemplate.opsForValue().get(String.format("user:refreshToken:%s", userId));
+        } catch (RedisConnectionFailureException ex) {
+            throw new ExternalServerException(ex.getMessage(), SERVICE_UNAVAILABLE);
+        } catch (TimeoutException ex) {
+            throw new ExternalServerException(ex.getMessage(), GATEWAY_TIMEOUT);
+        } catch (IllegalArgumentException ex) {
+            throw new ExternalServerException(ex.getMessage(), BAD_REQUEST);
+        } catch (SerializationException ex) {
+            throw new ExternalServerException(ex.getMessage(), INTERNAL_SERVER_ERROR);
         } catch (Exception ex) {
-            throw new ExternalServerException(REDIS_SEARCH_FAILED_MESSAGE, BAD_GATEWAY);
+            throw new ExternalServerException(ex.getMessage(), BAD_GATEWAY);
         }
     }
 }

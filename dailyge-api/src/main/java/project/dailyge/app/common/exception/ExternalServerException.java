550ac20ee0905e4a1481d7dec582da7ee19107b1
beatmejy <beatmejy@naver.com>
2024-07-04 14:15:58 +0900

    style: 안쓰는 코드 삭제.

--- dailyge-api/src/main/java/project/dailyge/app/common/exception/ExternalServerException.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/exception/ExternalServerException.java
@@ -4,9 +4,6 @@ import project.dailyge.app.common.codeandmessage.CodeAndMessage;
 
 public class ExternalServerException extends CommonException {
 
-    public static final String REDIS_SAVE_FAILED_MESSAGE = "레디스 데이터 저장에 실패하였습니다.";
-    public static final String REDIS_SEARCH_FAILED_MESSAGE = "레디스 데이터 조회에 실패하였습니다.";
-
     public ExternalServerException(
         String detailMessage,
         CodeAndMessage codeAndMessage

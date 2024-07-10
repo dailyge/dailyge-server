2fd6db9897b640f633a457cd17b83afe3459fea8
devjun10 <devjun1023@gmail.com>
2024-07-10 16:23:22 +0900

    feat: MongoDB Auditing 설정 추가 - CreatedAt, LastModifiedAt을 사용하기 위한 설정 추가

---
+++ storage/document/src/main/java/project/dailyge/document/configuration/MongoConfiguration.java
@@ -0,0 +1,9 @@
+package project.dailyge.document.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfiguration {
+}

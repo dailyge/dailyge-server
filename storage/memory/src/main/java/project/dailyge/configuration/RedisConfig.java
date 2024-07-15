d6f207c70eecaac1d2a6ceefd964882d69563de1
devjun10 <devjun1023@gmail.com>
2024-07-15 14:59:46 +0900

    refactor: Redis 모듈 설정 이동 - storage:memory로 위치 이동

--- storage/memory/src/main/java/project/dailyge/configuration/RedisConfig.java
+++ storage/memory/src/main/java/project/dailyge/configuration/RedisConfig.java
@@ -1,4 +1,4 @@
-package project.dailyge.app.common.configuration.redis;
+package project.dailyge.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;

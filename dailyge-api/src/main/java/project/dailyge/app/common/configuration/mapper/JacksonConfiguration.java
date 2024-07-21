b89ba7f7c9ac8d0b88fbd1ceb4e6ec395ec3b3bd
devjun10 <devjun1023@gmail.com>
2024-07-21 20:39:11 +0900

    refactor: 날짜 직렬화 설정 추가 - ObjectMapper 날짜 관련 설정 추가

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/mapper/JacksonConfiguration.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/mapper/JacksonConfiguration.java
@@ -1,21 +1,28 @@
 package project.dailyge.app.common.configuration.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class JacksonConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
+        final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new ParameterNamesModule());
         mapper.registerModule(new Jdk8Module());
         mapper.registerModule(new JavaTimeModule());
+        mapper.disable(WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
         return mapper;
     }
 }

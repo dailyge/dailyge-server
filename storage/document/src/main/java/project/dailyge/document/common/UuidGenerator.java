bf479a732b2b28db9d00a5f412c4ede6dbe803df
devjun10 <devjun1023@gmail.com>
2024-07-21 14:47:54 +0900

    chore: UUID 의존성 추가 - MongoDB에서 PK로 UUID 사용

---
+++ storage/document/src/main/java/project/dailyge/document/common/UuidGenerator.java
@@ -0,0 +1,19 @@
+package project.dailyge.document.common;
+
+import com.fasterxml.uuid.Generators;
+
+public final class UuidGenerator {
+
+    private UuidGenerator() {
+        throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
+    }
+
+    public static String createTimeBasedUUID() {
+        return Generators.timeBasedGenerator().generate().toString();
+    }
+
+    public static void main(String[] args) {
+        Generators.timeBasedGenerator().generate().toString();
+        System.out.println(Generators.timeBasedGenerator().generate().toString());
+    }
+}

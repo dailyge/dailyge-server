ce1fbedfde79ce37e1cd92d3e35383253da605fb
devjun10 <devjun1023@gmail.com>
2024-07-23 10:13:09 +0900

    test: Task 수정 통합 테스트 작성 - 테스트 케이스 보강 - Task 상태 업데이트 케이스 추가

--- dailyge-api/src/test/java/project/dailyge/app/test/task/integrationtest/TaskUpdateIntegrationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/task/integrationtest/TaskUpdateIntegrationTest.java
@@ -1,110 +1,105 @@
-//package project.dailyge.app.test.task.integrationtest;
-//
-//
-//import static java.time.LocalDate.now;
-//import static org.assertj.core.api.Assertions.assertThatThrownBy;
-//import static org.junit.jupiter.api.Assertions.assertAll;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import project.dailyge.app.common.DatabaseTestBase;
-//import project.dailyge.app.common.auth.DailygeUser;
-//import static project.dailyge.app.common.codeandmessage.CommonCodeAndMessage.UN_AUTHORIZED;
-//import project.dailyge.app.common.exception.UnAuthorizedException;
-//import project.dailyge.app.core.task.application.TaskReadUseCase;
-//import project.dailyge.app.core.task.application.TaskWriteUseCase;
-//import project.dailyge.app.core.task.application.command.TaskUpdateCommand;
-//import static project.dailyge.app.core.task.exception.TaskCodeAndMessage.TASK_NOT_FOUND;
-//import project.dailyge.app.core.task.exception.TaskTypeException;
-//import project.dailyge.app.core.user.application.UserWriteUseCase;
-//import static project.dailyge.app.fixture.user.UserFixture.createUserJpaEntity;
-//import project.dailyge.entity.task.TaskJpaEntity;
-//import static project.dailyge.entity.task.TaskStatus.IN_PROGRESS;
-//import static project.dailyge.entity.task.TaskStatus.TODO;
-//import static project.dailyge.entity.user.Role.NORMAL;
-//import project.dailyge.entity.user.UserJpaEntity;
-//
-//@DisplayName("[IntegrationTest] 할 일 수정 통합 테스트")
-//public class TaskUpdateIntegrationTest extends DatabaseTestBase {
-//
-//    @Autowired
-//    private TaskReadUseCase taskReadUseCase;
-//
-//    @Autowired
-//    private TaskWriteUseCase taskWriteUseCase;
-//
-//    @Autowired
-//    private UserWriteUseCase userWriteUseCase;
-//
-//    @Test
-//    @DisplayName("할 일을 업데이트하면, 내용이 반영된다.")
-//    void whenTaskUpdateThenContentsShouldBeHappen() {
-//        final UserJpaEntity newUser = userWriteUseCase.save(createUserJpaEntity());
-//        final TaskJpaEntity newTask = new TaskJpaEntity(
-//            "독서",
-//            "Kafka 완벽가이드 1~30p 읽기",
-//            now(),
-//            TODO,
-//            newUser.getId()
-//        );
-//        final TaskJpaEntity savedTask = taskWriteUseCase.save(newTask);
-//
-//        final DailygeUser dailygeUser = new DailygeUser(newUser.getId(), NORMAL);
-//        final TaskUpdateCommand command = new TaskUpdateCommand(
-//            "미팅",
-//            "Backend 부서와 10시 미팅",
-//            now(),
-//            IN_PROGRESS
-//        );
-//        taskWriteUseCase.update(dailygeUser, savedTask.getId(), command);
-//
-//        final TaskJpaEntity findTask = taskReadUseCase.findById(dailygeUser, savedTask.getId());
-//        assertAll(
-//            () -> assertEquals(command.title(), findTask.getTitle()),
-//            () -> assertEquals(command.content(), findTask.getContent()),
-//            () -> assertEquals(command.date(), findTask.getDate()),
-//            () -> assertEquals(command.status(), findTask.getStatus())
-//        );
-//    }
-//
-//    @Test
-//    @DisplayName("존재하지 않는 사용자 ID가 입력되면, UnAuthorizedException이 발생한다.")
-//    void whenInvalidUserIdThenUnAuthorizedExceptionShouldBeHappen() {
-//        final UserJpaEntity newUser = userWriteUseCase.save(createUserJpaEntity());
-//        final TaskJpaEntity newTask = new TaskJpaEntity("독서", "Kafka 완벽가이드 1~30p 읽기", now(), TODO, newUser.getId());
-//        final TaskJpaEntity savedTask = taskWriteUseCase.save(newTask);
-//
-//        final Long invalidUserId = Long.MAX_VALUE;
-//        final DailygeUser dailygeUser = new DailygeUser(invalidUserId, NORMAL);
-//        final TaskUpdateCommand command = new TaskUpdateCommand(
-//            "미팅",
-//            "Backend 부서와 10시 미팅",
-//            now(),
-//            IN_PROGRESS
-//        );
-//        assertThatThrownBy(() -> taskWriteUseCase.update(dailygeUser, savedTask.getId(), command))
-//            .isInstanceOf(UnAuthorizedException.class)
-//            .hasMessage(UN_AUTHORIZED.message());
-//    }
-//
-//    @Test
-//    @DisplayName("존재하지 않는 할 일 ID가 입력되면, TaskTypeException이 발생한다.")
-//    void whenInvalidTaskIdThenTaskTypeExceptionShouldBeHappen() {
-//        final UserJpaEntity newUser = userWriteUseCase.save(createUserJpaEntity());
-//        final TaskJpaEntity newTask = new TaskJpaEntity("독서", "Kafka 완벽가이드 1~30p 읽기", now(), TODO, newUser.getId());
-//        taskWriteUseCase.save(newTask);
-//
-//        final Long invalidTaskId = Long.MAX_VALUE;
-//        final DailygeUser dailygeUser = new DailygeUser(newUser.getId(), NORMAL);
-//        final TaskUpdateCommand command = new TaskUpdateCommand(
-//            "미팅",
-//            "Backend 부서와 10시 미팅",
-//            now(),
-//            IN_PROGRESS
-//        );
-//        assertThatThrownBy(() -> taskWriteUseCase.update(dailygeUser, invalidTaskId, command))
-//            .isInstanceOf(TaskTypeException.class)
-//            .hasMessage(TASK_NOT_FOUND.message());
-//    }
-//}
+package project.dailyge.app.test.task.integrationtest;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import project.dailyge.app.common.DatabaseTestBase;
+import project.dailyge.app.common.auth.DailygeUser;
+import project.dailyge.app.core.task.application.TaskWriteUseCase;
+import project.dailyge.app.core.task.application.command.TaskCreateCommand;
+import project.dailyge.app.core.task.application.command.TaskUpdateCommand;
+import static project.dailyge.app.core.task.exception.TaskCodeAndMessage.TASK_NOT_FOUND;
+import project.dailyge.app.core.task.exception.TaskTypeException;
+import project.dailyge.app.core.task.facade.TaskFacade;
+import static project.dailyge.app.fixture.user.UserFixture.createAdminUser;
+import static project.dailyge.app.fixture.user.UserFixture.createUserJpaEntity;
+import static project.dailyge.app.test.task.fixture.TaskCommandFixture.createTaskCreationCommand;
+import static project.dailyge.app.test.task.fixture.TaskCommandFixture.createTaskUpdateCommand;
+import project.dailyge.document.task.MonthlyTaskDocument;
+import project.dailyge.document.task.TaskDocument;
+import project.dailyge.document.task.TaskDocumentReadRepository;
+import static project.dailyge.entity.user.Role.NORMAL;
+import project.dailyge.entity.user.UserJpaEntity;
+
+import java.time.LocalDate;
+
+@DisplayName("[IntegrationTest] 할 일 수정 통합 테스트")
+class TaskUpdateIntegrationTest extends DatabaseTestBase {
+
+    private MonthlyTaskDocument findMonthlyTask;
+    private TaskCreateCommand taskCreateCommand;
+
+    @Autowired
+    private TaskWriteUseCase taskWriteUseCase;
+
+    @Autowired
+    private TaskFacade taskFacade;
+
+    @Autowired
+    private TaskDocumentReadRepository taskReadRepository;
+
+    @BeforeEach
+    void setUp() {
+        newUser = userWriteUseCase.save(createUserJpaEntity());
+        dailygeUser = new DailygeUser(newUser);
+        now = LocalDate.now();
+        taskFacade.createMonthlyTasks(dailygeUser, now);
+        findMonthlyTask = taskReadRepository.findMonthlyDocumentByUserIdAndDate(dailygeUser.getUserId(), now).get();
+        taskCreateCommand = createTaskCreationCommand(findMonthlyTask.getId());
+    }
+
+    @Test
+    @DisplayName("Task를 업데이트하면, 내용이 반영된다.")
+    void whenTaskUpdateThenContentsShouldBeHappen() {
+        final String newTaskId = taskFacade.save(dailygeUser, taskCreateCommand);
+        final TaskUpdateCommand taskUpdateCommand = createTaskUpdateCommand(findMonthlyTask.getId());
+        taskWriteUseCase.update(dailygeUser, newTaskId, taskUpdateCommand);
+
+        final TaskDocument findTaskDocument = taskReadRepository.findTaskDocumentByIds(
+            taskUpdateCommand.monthlyTaskId(),
+            newTaskId
+        ).orElseThrow(() -> TaskTypeException.from(TASK_NOT_FOUND));
+
+        assertAll(
+            () -> assertEquals(taskUpdateCommand.monthlyTaskId(), findTaskDocument.getMonthlyTaskId()),
+            () -> assertEquals(taskUpdateCommand.title(), findTaskDocument.getTitle()),
+            () -> assertEquals(taskUpdateCommand.content(), findTaskDocument.getContent()),
+            () -> assertEquals(taskUpdateCommand.getStatus(), findTaskDocument.getStatus()),
+            () -> assertEquals(taskUpdateCommand.date(), findTaskDocument.getLocalDate())
+        );
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 사용자 ID가 입력되면, TaskTypeException이 발생한다.")
+    void whenInvalidUserIdThenUnAuthorizedExceptionShouldBeHappen() {
+        final Long invalidUserId = 100_000L;
+        final String newTaskId = taskFacade.save(dailygeUser, taskCreateCommand);
+        final DailygeUser dailygeUser = new DailygeUser(invalidUserId, NORMAL);
+
+        final TaskUpdateCommand taskUpdateCommand = createTaskUpdateCommand(findMonthlyTask.getId());
+        assertThatThrownBy(() -> taskWriteUseCase.update(dailygeUser, newTaskId, taskUpdateCommand))
+            .isInstanceOf(TaskTypeException.class);
+    }
+
+    @Test
+    @DisplayName("관리자가 Task를 수정하면 권한 예외가 발생하지 않는다.")
+    void whenAdminUpdateTaskThenExceptionShouldNotBeHappen() {
+        final UserJpaEntity newUser = userWriteUseCase.save(createAdminUser("dailyge", "dailyge2024@gmail.com"));
+        final DailygeUser dailygeUser = new DailygeUser(newUser);
+        final LocalDate now = LocalDate.now();
+        taskFacade.createMonthlyTasks(dailygeUser, now);
+
+        final MonthlyTaskDocument findMonthlyTask = taskReadRepository.findMonthlyDocumentByUserIdAndDate(dailygeUser.getUserId(), now).get();
+        taskCreateCommand = createTaskCreationCommand(findMonthlyTask.getId());
+        final String newTaskId = taskFacade.save(dailygeUser, taskCreateCommand);
+
+        final TaskUpdateCommand taskUpdateCommand = createTaskUpdateCommand(findMonthlyTask.getId());
+        assertDoesNotThrow(() -> taskWriteUseCase.update(dailygeUser, newTaskId, taskUpdateCommand));
+    }
+}

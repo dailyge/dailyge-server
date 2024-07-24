4ab959f2356904ef7c6a4308b016611a3539d2fa
devjun10 <devjun1023@gmail.com>
2024-07-25 02:02:24 +0900

    feat: CodeAndMessage 저장 Api 추가 - CodeAndMessage를 데이터베이스에서 관리하도록 수정

---
+++ dailyge-api/src/main/java/project/dailyge/app/CommonApi.java
@@ -0,0 +1,36 @@
+package project.dailyge.app;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.OK;
+import project.dailyge.app.common.auth.DailygeUser;
+import project.dailyge.app.common.auth.LoginUser;
+import project.dailyge.app.common.codeandmessage.CodeAndMessageConfig;
+import project.dailyge.app.common.response.ApiResponse;
+import project.dailyge.entity.codeandmessage.CodeAndMessageEntityWriteService;
+import project.dailyge.entity.codeandmessage.CodeAndMessages;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = {"/api"})
+public class CommonApi {
+
+    private final CodeAndMessageEntityWriteService codeAndMessageWriteService;
+    private final CodeAndMessageConfig codeAndMessageConfig;
+
+    @GetMapping
+    public ApiResponse<String> healthCheck() {
+        return ApiResponse.from(OK);
+    }
+
+    // TODO. 추후 권한 점검 추가.
+    @PostMapping(path = {"/status"})
+    public ApiResponse<Void> saveAll(@LoginUser final DailygeUser dailygeUser) {
+        final CodeAndMessages codeAndMessages = new CodeAndMessages(codeAndMessageConfig.getAllCodeAndMessages());
+        codeAndMessageWriteService.saveAll(codeAndMessages);
+        return ApiResponse.from(OK);
+    }
+}

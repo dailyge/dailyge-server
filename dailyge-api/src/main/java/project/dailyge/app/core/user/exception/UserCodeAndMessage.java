bf76896a1e061612476e6447924dbdb734d7d27c
beatmejy <beatmejy@naver.com>
2024-07-26 13:55:24 +0900

    refactor: 유저 예외처리 리팩토링.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/exception/UserCodeAndMessage.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/exception/UserCodeAndMessage.java
@@ -5,7 +5,7 @@ import project.dailyge.app.common.codeandmessage.CodeAndMessage;
 public enum UserCodeAndMessage implements CodeAndMessage {
 
     USER_NOT_FOUND(404, "존재하지 않는 사용자 정보 입니다."),
-    ACTIVE_USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
+    ACTIVE_USER_NOT_FOUND(404, "삭제되지 않은 사용자 정보를 찾을 수 없습니다."),
     EMPTY_USER_ID(404, "사용자 ID가 존재하지 않습니다."),
     DUPLICATED_EMAIL(422, "이미 존재하는 이메일 계정입니다");
 

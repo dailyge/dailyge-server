e0dabd14e6075144c42bf09a6125725b6ef36e60
devjun10 <devjun1023@gmail.com>
2024-07-30 05:59:54 +0900

    feat: AWS SNS, SQS 설정 추가 - SNS, SQS 클라이언트 설정 추가

---
+++ admin-api/src/main/java/project/dailyge/app/common/configuration/aws/sqs/AmazonSqsConfiguration.java
@@ -0,0 +1,34 @@
+package project.dailyge.app.common.configuration.aws.sqs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Configuration
+public class AmazonSqsConfiguration {
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public SqsClient sqsClient() {
+        final AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        final StaticCredentialsProvider provider = StaticCredentialsProvider.create(awsBasicCredentials);
+        return SqsClient.builder()
+            .credentialsProvider(provider)
+            .httpClient(UrlConnectionHttpClient.builder().build())
+            .region(Region.of(region))
+            .build();
+    }
+}

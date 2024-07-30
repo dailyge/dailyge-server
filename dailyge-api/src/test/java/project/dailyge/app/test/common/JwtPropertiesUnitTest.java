81ad4d2c7b5e2244a1c6347d310b24df27c37eb5
beatmejy <beatmejy@naver.com>
2024-07-30 16:26:49 +0900

    test: SecretKeyManager 단위 테스트 작성.

--- dailyge-api/src/test/java/project/dailyge/app/test/common/JwtPropertiesUnitTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/common/JwtPropertiesUnitTest.java
@@ -1,6 +1,5 @@
 package project.dailyge.app.test.common;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

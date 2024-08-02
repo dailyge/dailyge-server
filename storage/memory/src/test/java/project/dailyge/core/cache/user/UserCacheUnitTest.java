a588b9053834b307c42ebeb225a66380e7661f08
beatmejy <beatmejy@naver.com>
2024-08-02 15:47:28 +0900

    test: UserCache 단위 테스트 작성.

---
+++ storage/memory/src/test/java/project/dailyge/core/cache/user/UserCacheUnitTest.java
@@ -0,0 +1,28 @@
+package project.dailyge.core.cache.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("[UnitTest] UserCache 단위 테스트")
+public class UserCacheUnitTest {
+
+    @Test
+    @DisplayName("사용자 Cache 를 생성하면, 정상적으로 Cache 가 생성된다.")
+    void whenCreateUserCacheThenResultShouldBeNotNull() {
+        final Long id = 1L;
+        final String nickname = "test";
+        final String email = "test@gmail.com";
+        final String profileImage = "test@gmail.com";
+        final UserCache userCache = new UserCache(id, nickname, email, profileImage);
+
+        assertAll(
+            () -> assertEquals(id, userCache.getId()),
+            () -> assertEquals(nickname, userCache.getNickname()),
+            () -> assertEquals(email, userCache.getEmail()),
+            () -> assertEquals(profileImage, userCache.getProfileImageUrl())
+        );
+    }
+}

b872b893b9498ad5f7416307f9d6629a397a3f31
beatmejy <beatmejy@naver.com>
2024-08-02 16:50:00 +0900

    refactor: UserCache 저장 비동기 처리.

--- dailyge-api/src/main/java/project/dailyge/app/core/user/facade/UserFacade.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/facade/UserFacade.java
@@ -1,11 +1,12 @@
 package project.dailyge.app.core.user.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import project.dailyge.app.common.auth.DailygeToken;
 import project.dailyge.app.common.auth.TokenProvider;
 import project.dailyge.app.common.exception.CommonException;
-import project.dailyge.app.core.user.application.UserCacheWriteUseCase;
+import project.dailyge.core.cache.user.UserCacheEvent;
 import project.dailyge.app.core.user.application.UserReadUseCase;
 import project.dailyge.app.core.user.application.UserWriteUseCase;
 import project.dailyge.app.core.user.external.oauth.GoogleOAuthManager;
@@ -25,20 +26,19 @@ public class UserFacade {
     private final UserReadUseCase userReadUseCase;
     private final TokenProvider tokenProvider;
     private final TokenManager tokenManager;
-    private final UserCacheWriteUseCase userCacheWriteUseCase;
+    private final ApplicationEventPublisher publisher;
 
     public DailygeToken login(final String code) throws CommonException {
         final GoogleUserInfoResponse response = googleOAuthManager.getUserInfo(code);
         final Optional<UserJpaEntity> findUserByEmail = userReadUseCase.findActiveUserByEmail(response.getEmail());
-        final UserCache userCache = getUserCache(findUserByEmail, response);
-        userCacheWriteUseCase.save(userCache);
+        final UserCache userCache = publishUserSave(findUserByEmail, response);
 
         final DailygeToken token = tokenProvider.createToken(userCache.getId(), response.getEmail());
         tokenManager.saveRefreshToken(userCache.getId(), token.refreshToken());
         return token;
     }
 
-    private UserCache getUserCache(
+    private UserCache publishUserSave(
         final Optional<UserJpaEntity> findUserByEmail,
         final GoogleUserInfoResponse response
     ) {
@@ -46,11 +46,12 @@ public class UserFacade {
         if (findUserByEmail.isEmpty()) {
             final Long newUserSequence = userWriteUseCase.getSequence();
             userCache = new UserCache(newUserSequence, response.getName(), response.getEmail(), response.getPicture());
-            // TODO - 지율 여기에 추후 메시지큐 전송 로직 추가 예정.
+            // TODO - 여기에 추후 메시지큐 전송 로직 추가 예정.
         } else {
             final UserJpaEntity user = findUserByEmail.get();
             userCache = new UserCache(user.getId(), user.getNickname(), user.getEmail(), user.getProfileImageUrl());
         }
+        publisher.publishEvent(new UserCacheEvent(userCache));
         return userCache;
     }
 
---
+++ storage/memory/src/main/java/project/dailyge/core/cache/user/UserCacheEvent.java
@@ -0,0 +1,4 @@
+package project.dailyge.core.cache.user;
+
+public record UserCacheEvent(UserCache userCache) {
+}

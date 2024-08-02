8b002c884aa7b094821dc66ddb6cc62a66c1925a
beatmejy <beatmejy@naver.com>
2024-08-02 14:25:07 +0900

    feat: RedisConfig UserCache 추가.

--- storage/memory/src/main/java/project/dailyge/common/configuration/RedisConfig.java
+++ storage/memory/src/main/java/project/dailyge/common/configuration/RedisConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import project.dailyge.core.cache.user.UserCache;
 
 @Configuration
 public class RedisConfig {
@@ -29,5 +32,14 @@ public class RedisConfig {
         stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
         stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
         return stringRedisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, UserCache> userCacheRedisTemplate() {
+        RedisTemplate<String, UserCache> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
     }
 }

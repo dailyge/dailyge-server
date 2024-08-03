9d62cf970d1fc5f5c5660f5c5d5489268cdc1e65
beatmejy <beatmejy@naver.com>
2024-08-03 17:57:10 +0900

    feat: Corsfilter 로직 구현

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/CorsFilter.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/CorsFilter.java
@@ -5,22 +5,50 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Order(2)
+import static jakarta.servlet.http.HttpServletResponse.SC_OK;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_MAX_AGE;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpHeaders.ORIGIN;
+import static org.springframework.http.HttpMethod.OPTIONS;
+
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    // TODO. CorsFilter 추가.
     @Override
     public void doFilter(
         final ServletRequest servletRequest,
         final ServletResponse servletResponse,
         final FilterChain filterChain
-    ) throws IOException, ServletException {
+    ) throws ServletException, IOException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        final String origin = request.getHeader(ORIGIN);
+        response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+        response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, "*");
+        response.setHeader(ACCESS_CONTROL_MAX_AGE, "3600");
+        response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS,
+            String.join(", ", ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, "X-Requested-With"));
 
+        if (OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(SC_OK);
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }

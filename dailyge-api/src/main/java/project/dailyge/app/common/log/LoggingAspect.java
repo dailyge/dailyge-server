1e189d102819c5de2c111aa34cdb12c729c72b97
devjun10 <devjun1023@gmail.com>
2024-08-05 16:51:56 +0900

    feat: Log 설정 추가 - 각 레이어마다 로그를 남기도록 설정 변경

---
+++ dailyge-api/src/main/java/project/dailyge/app/common/log/LoggingAspect.java
@@ -0,0 +1,104 @@
+package project.dailyge.app.common.log;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import project.dailyge.app.common.auth.DailygeUser;
+import project.dailyge.app.utils.LogUtils;
+import static project.dailyge.app.utils.LogUtils.getGuest;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final String SERVER = "dailyge-api";
+    private static final String PRESENTATION = "PRESENTATION";
+    private static final String FACADE = "FACADE";
+    private static final String APPLICATION = "APPLICATION";
+    private static final String EXTERNAL = "EXTERNAL";
+    private static final String TRACE_ID = "traceId";
+    private static final String IP = "ip";
+
+    @Around("@within(project.dailyge.app.common.annotation.Presentation)")
+    public Object writeLogAroundPresentation(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return writeLog(joinPoint, PRESENTATION);
+    }
+
+    @Around("@within(project.dailyge.app.common.annotation.Facade)")
+    public Object writeLogAroundFacade(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return writeLog(joinPoint, FACADE);
+    }
+
+    @Around("@within(project.dailyge.app.common.annotation.Application)")
+    public Object writeLogAroundApplication(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return writeLog(joinPoint, APPLICATION);
+    }
+
+    @Around("@within(project.dailyge.app.common.annotation.External)")
+    public Object writeLogAroundExternal(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return writeLog(joinPoint, EXTERNAL);
+    }
+
+    private Object writeLog(
+        final ProceedingJoinPoint joinPoint,
+        final String layer
+    ) throws Throwable {
+        final String traceId = MDC.get(TRACE_ID);
+        final LocalDateTime startTime = LocalDateTime.now();
+        final String args = Arrays.toString(joinPoint.getArgs());
+        final String beforeLog = LogUtils.createLogMessage(
+            SERVER,
+            traceId,
+            getIp(),
+            layer,
+            getDailygeUserAsString(),
+            startTime,
+            0,
+            args,
+            null
+        );
+        log.info(beforeLog);
+
+        final Object result = joinPoint.proceed();
+        final LocalDateTime endTime = LocalDateTime.now();
+        long duration = ChronoUnit.MILLIS.between(startTime, endTime);
+        final String afterLog = LogUtils.createLogMessage(
+            SERVER,
+            traceId,
+            getIp(),
+            layer,
+            getDailygeUserAsString(),
+            endTime,
+            duration,
+            args,
+            result
+        );
+        log.info(afterLog);
+        return result;
+    }
+
+    private String getIp() {
+        return MDC.get(IP);
+    }
+
+    private String getDailygeUserAsString() {
+        final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        final Object dailyObj = request.getAttribute("dailyge-user");
+        if (dailyObj != null) {
+            final DailygeUser dailygeUser = (DailygeUser) dailyObj;
+            return dailygeUser.toString();
+        }
+        return getGuest();
+    }
+}
---
+++ support/src/main/java/project/dailyge/app/utils/LogUtils.java
@@ -0,0 +1,117 @@
+package project.dailyge.app.utils;
+
+import com.fasterxml.uuid.Generators;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class LogUtils {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+    private static final String GUEST = "{ \"userId\":null, \"role\":\"GUEST\" }";
+    private static final String EMPTY_STRING = "null";
+
+    private LogUtils() {
+        throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
+    }
+
+    public static String createLogMessage(
+        final String server,
+        final String traceId,
+        final String ipAddress,
+        final String layer,
+        final String visitor,
+        final LocalDateTime time,
+        final long duration,
+        final Object args,
+        final Object result
+    ) {
+        final String formattedTime = time.format(DATE_TIME_FORMATTER);
+        final String argsString = (args != null) ? args.toString() : EMPTY_STRING;
+        final String resultString = (result != null) ? result.toString() : EMPTY_STRING;
+
+        return String.format(
+            "{\"server\":\"%s\", \"traceId\":\"%s\", \"ip\":\"%s\", \"layer\":\"%s\", \"visitor\":\"%s\", \"time\":\"%s\", \"duration\":\"%dms\", \"context\":{\"args\":%s, \"result\":%s}}",
+            server,
+            traceId,
+            ipAddress,
+            layer,
+            getVisitor(visitor),
+            formattedTime,
+            duration,
+            argsString,
+            resultString
+        );
+    }
+
+    public static String createLogMessage(
+        final String server,
+        final String ipAddress,
+        final String layer,
+        final String visitor,
+        final LocalDateTime time,
+        final long duration,
+        final Object args,
+        final Object result
+    ) {
+        final String formattedTime = time.format(DATE_TIME_FORMATTER);
+        final String argsString = (args != null) ? args.toString() : EMPTY_STRING;
+        final String resultString = (result != null) ? result.toString() : EMPTY_STRING;
+
+        return String.format(
+            "{\"server\":\"%s\", \"traceId\":\"%s\", \"ip\":\"%s\", \"layer\":\"%s\", \"visitor\":\"%s\", \"time\":\"%s\", \"duration\":\"%dms\", \"context\":{\"args\":%s, \"result\":%s}}",
+            server,
+            createTraceId(),
+            ipAddress,
+            layer,
+            getVisitor(visitor),
+            formattedTime,
+            duration,
+            argsString,
+            resultString
+        );
+    }
+
+    public static String createGuestLogMessage(
+        final String server,
+        final String traceId,
+        final String ipAddress,
+        final String layer,
+        final LocalDateTime time,
+        final long duration,
+        final Object args,
+        final Object result
+    ) {
+        final String formattedTime = time.format(DATE_TIME_FORMATTER);
+        final String argsString = (args != null) ? args.toString() : EMPTY_STRING;
+        final String resultString = (result != null) ? result.toString() : EMPTY_STRING;
+
+        return String.format(
+            "{\"server\":\"%s\", \"traceId\":\"%s\", \"ip\":\"%s\", \"layer\":\"%s\", \"visitor\":\"%s\", \"time\":\"%s\", \"duration\":\"%dms\", \"context\":{\"args\":%s, \"result\":%s}}",
+            server,
+            traceId,
+            ipAddress,
+            layer,
+            GUEST,
+            formattedTime,
+            duration,
+            argsString,
+            resultString
+        );
+    }
+
+    private static String createTraceId() {
+        return Generators.timeBasedGenerator().generate().toString();
+    }
+
+    public static String getGuest() {
+        return GUEST;
+    }
+
+    private static String getVisitor(final String visitor) {
+        if (visitor == null || visitor.isBlank()) {
+            return GUEST;
+        }
+        return visitor;
+    }
+}

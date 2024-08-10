8c88a28280f8889876404189151d4a2438be2339
jiyoul <beatmejy@naver.com>
2024-08-10 19:01:14 +0900

    test: UserFixture와 테스트 작성 시 이메일 구분  - UserFixture는 test@gmail.com  - 테스트 작성 시에는 dailyges@gmail.com

--- dailyge-api/src/test/java/project/dailyge/app/test/user/fixture/UserFixture.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/user/fixture/UserFixture.java
@@ -9,32 +9,26 @@ public final class UserFixture {
         throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
     }
 
-    public static final String NICKNAME = "dailyge";
-    public static final String EMAIL = "dailyge@gmail.com";
+    public static final String NICKNAME = "test";
+    public static final String EMAIL = "test@gmail.com";
 
     public static UserJpaEntity createUser(final Long userId) {
         return new UserJpaEntity(userId, NICKNAME, EMAIL);
     }
 
-    public static UserJpaEntity createUser() {
-        return new UserJpaEntity(NICKNAME, EMAIL);
-    }
-
     public static UserJpaEntity createUser(
+        final Long userId,
         final String nickname,
         final String email
     ) {
-        return new UserJpaEntity(nickname, email);
-    }
-
-    public static UserJpaEntity createAdminUser() {
-        return new UserJpaEntity(NICKNAME, EMAIL, ADMIN);
+        return new UserJpaEntity(userId, nickname, email);
     }
 
     public static UserJpaEntity createAdminUser(
+        final Long id,
         final String nickname,
         final String email
     ) {
-        return new UserJpaEntity(nickname, email, ADMIN);
+        return new UserJpaEntity(id, nickname, email, ADMIN);
     }
 }

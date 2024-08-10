6cef69587502891a07349af6c1d46c08a0ddab1a
jiyoul <beatmejy@naver.com>
2024-08-10 16:11:10 +0900

    chore: 병합과정에서 충돌나는 파일 임시 코드 작성

--- dailyge-api/src/main/java/project/dailyge/app/core/user/facade/UserFacade.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/facade/UserFacade.java
@@ -14,7 +14,6 @@ import project.dailyge.app.core.user.external.response.GoogleUserInfoResponse;
 import project.dailyge.core.cache.user.UserCache;
 import project.dailyge.core.cache.user.UserCacheWriteUseCase;
 import project.dailyge.entity.user.UserEvent;
-
 import static project.dailyge.document.common.UuidGenerator.createTimeBasedUUID;
 import static project.dailyge.entity.common.EventType.CREATE;
 import static project.dailyge.entity.user.Role.NORMAL;
@@ -24,29 +23,29 @@ import static project.dailyge.entity.user.UserEvent.createEvent;
 @RequiredArgsConstructor
 public class UserFacade {
 
-	private final GoogleOAuthManager googleOAuthManager;
-	private final UserWriteUseCase userWriteUseCase;
-	private final UserReadUseCase userReadUseCase;
-	private final TokenProvider tokenProvider;
-	private final TokenManager tokenManager;
-	private final ApplicationEventPublisher eventPublisher;
-	private final UserCacheWriteUseCase userCacheWriteUseCase;
+    private final GoogleOAuthManager googleOAuthManager;
+    private final UserWriteUseCase userWriteUseCase;
+    private final UserReadUseCase userReadUseCase;
+    private final TokenProvider tokenProvider;
+    private final TokenManager tokenManager;
+    private final ApplicationEventPublisher eventPublisher;
+    private final UserCacheWriteUseCase userCacheWriteUseCase;
 
-	public DailygeToken login(final String code) throws CommonException {
-		final GoogleUserInfoResponse response = googleOAuthManager.getUserInfo(code);
-		final Long findUserId = userReadUseCase.findUserIdByEmail(response.getEmail());
-		final Long userId = saveCache(findUserId, response);
-		final UserEvent userEvent = createEvent(userId, createTimeBasedUUID(), CREATE);
-		eventPublisher.publishEvent(userEvent);
+    public DailygeToken login(final String code) throws CommonException {
+        final GoogleUserInfoResponse response = googleOAuthManager.getUserInfo(code);
+        final Long findUserId = userReadUseCase.findUserIdByEmail(response.getEmail());
+        final Long userId = saveCache(findUserId, response);
+        final UserEvent userEvent = createEvent(userId, createTimeBasedUUID(), CREATE);
+        eventPublisher.publishEvent(userEvent);
 
-		final DailygeToken token = tokenProvider.createToken(userId, response.getEmail());
-		tokenManager.saveRefreshToken(userId, token.refreshToken());
-		return token;
-	}
+        final DailygeToken token = tokenProvider.createToken(userId, response.getEmail());
+        tokenManager.saveRefreshToken(userId, token.refreshToken());
+        return token;
+    }
 
-	private Long saveCache(Long userId, GoogleUserInfoResponse response) {
-		final UserCache userCache;
-		if (userId != null) {
+    private Long saveCache(Long userId, GoogleUserInfoResponse response) {
+        final UserCache userCache;
+        if (userId != null) {
             userCache = new UserCache(
                 userId,
                 response.getName(),
@@ -54,22 +53,22 @@ public class UserFacade {
                 response.getPicture(),
                 NORMAL.name()
             );
-		} else {
-			userCache = new UserCache(
+        } else {
+            userCache = new UserCache(
                 userWriteUseCase.getSequence(),
                 response.getName(),
                 response.getEmail(),
                 response.getPicture(),
                 NORMAL.name()
-			);
-		}
-		userCacheWriteUseCase.save(userCache);
-		return userCache.getId();
-	}
+            );
+        }
+        userCacheWriteUseCase.save(userCache);
+        return userCache.getId();
+    }
 
-	public void logout(final Long userId) {
-		if (userId != null) {
-			tokenManager.deleteRefreshToken(userId);
-		}
-	}
+    public void logout(final Long userId) {
+        if (userId != null) {
+            tokenManager.deleteRefreshToken(userId);
+        }
+    }
 }

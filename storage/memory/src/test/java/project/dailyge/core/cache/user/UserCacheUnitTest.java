5ab635b99dd3b8bd47c5a8f64c5288ed16532e2a
beatmejy <beatmejy@naver.com>
2024-08-11 16:33:33 +0900

    test: 테스트 반복되는 객체 생성해두고 사용

--- storage/memory/src/test/java/project/dailyge/core/cache/user/UserCacheUnitTest.java
+++ storage/memory/src/test/java/project/dailyge/core/cache/user/UserCacheUnitTest.java
@@ -1,6 +1,6 @@
 package project.dailyge.core.cache.user;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -12,15 +12,17 @@ class UserCacheUnitTest {
 
     private static final long ID = 1L;
     private static final long ANOTHER_ID = 2L;
-    private static final String NICKNAME = "test";
-    private static final String EMAIL = "test@gmail.com";
+    private static final String NICKNAME = "dailyges";
+    private static final String EMAIL = "dailyges@gmail.com";
     private static final String PROFILE_IMAGE_URL = "profile.jpg";
     private static final String ROLE = "NORMAL";
 
-    private UserCache userCache;
+    private static UserCache userCache;
+    private static UserCache userCacheWithId1;
+    private static UserCache userCacheWithId2;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         userCache = new UserCache(
             ID,
             NICKNAME,
@@ -28,8 +30,21 @@ class UserCacheUnitTest {
             PROFILE_IMAGE_URL,
             ROLE
         );
+        userCacheWithId1 = new UserCache(
+            ID,
+            NICKNAME,
+            EMAIL,
+            PROFILE_IMAGE_URL,
+            ROLE
+        );
+        userCacheWithId2 = new UserCache(
+            ANOTHER_ID,
+            NICKNAME,
+            EMAIL,
+            PROFILE_IMAGE_URL,
+            ROLE
+        );
     }
-
 
     @Test
     @DisplayName("사용자 Cache 를 생성하면, 정상적으로 Cache 가 생성된다.")
@@ -46,56 +61,24 @@ class UserCacheUnitTest {
     @Test
     @DisplayName("사용자 ID가 같다면, 동일한 객체이다.")
     void whenUserIdSameThenInstanceShouldBeSame() {
-        final UserCache userCacheWithId1 = new UserCache(
-            ID,
-            NICKNAME,
-            EMAIL,
-            PROFILE_IMAGE_URL,
-            ROLE
-        );
-
         assertEquals(userCache, userCacheWithId1);
     }
 
     @Test
     @DisplayName("사용자 ID가 다르다면, 다른 객체이다.")
     void whenUserIdDifferentThenInstanceShouldBeDifferent() {
-        final UserCache userCacheWithId2 = new UserCache(
-            ANOTHER_ID,
-            NICKNAME,
-            EMAIL,
-            PROFILE_IMAGE_URL,
-            ROLE
-        );
-
         assertNotEquals(userCache.hashCode(), userCacheWithId2.hashCode());
     }
 
     @Test
     @DisplayName("사용자 ID가 같다면, 동일한 해시코드를 갖는다.")
     void whenUserIdSameThenHashcodeShouldBeSame() {
-        final UserCache userCacheWithId1 = new UserCache(
-            ID,
-            NICKNAME,
-            EMAIL,
-            PROFILE_IMAGE_URL,
-            ROLE
-        );
-
         assertEquals(userCache.hashCode(), userCacheWithId1.hashCode());
     }
 
     @Test
     @DisplayName("사용자 ID가 다르다면, 다른 해시코드를 갖는다.")
     void whenUserIdDifferentThenHashcodeShouldBeDifferent() {
-        final UserCache userCacheWithId2 = new UserCache(
-            ANOTHER_ID,
-            NICKNAME,
-            EMAIL,
-            PROFILE_IMAGE_URL,
-            ROLE
-        );
-
         assertNotEquals(userCache.hashCode(), userCacheWithId2.hashCode());
     }
 }

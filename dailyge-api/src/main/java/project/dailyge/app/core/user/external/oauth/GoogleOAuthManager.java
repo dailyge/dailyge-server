854edd68834ea9e8d9db1b3178253c143371f656
devjun10 <devjun1023@gmail.com>
2024-08-14 16:40:18 +0900

    refactor: 부하 테스트를 위해 모킹 설정 추가 - dev 환경일 경우, 목 데이터를 반환하도록 변경

--- dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/GoogleOAuthManager.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/GoogleOAuthManager.java
@@ -1,41 +1,63 @@
 package project.dailyge.app.core.user.external.oauth;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpMethod.GET;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.BAD_GATEWAY;
+import project.dailyge.app.common.annotation.ExternalLayer;
 import project.dailyge.app.common.exception.ExternalServerException;
+import static project.dailyge.app.core.user.external.oauth.OAuthClient.GOOGLE;
 import project.dailyge.app.core.user.external.request.GoogleAuthorizationRequest;
 import project.dailyge.app.core.user.external.response.GoogleAuthorizationResponse;
 import project.dailyge.app.core.user.external.response.GoogleUserInfoResponse;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpMethod.GET;
-import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.BAD_GATEWAY;
-import static project.dailyge.app.core.user.external.oauth.OAuthClient.GOOGLE;
+import java.util.UUID;
 
-@Component
-@RequiredArgsConstructor
+@ExternalLayer
 public class GoogleOAuthManager {
 
-    @Value("${oauth.google.authorization}")
-    private String authorizationUrl;
-
-    @Value("${oauth.google.user-access}")
-    private String userAccessUrl;
     private static final String BEARER = "Bearer ";
 
+    private final String env;
+    private final String authorizationUrl;
+    private final String userAccessUrl;
     private final RestTemplate restTemplate;
     private final ClientRegistrationRepository clientRegistrationRepository;
 
+    public GoogleOAuthManager(
+        @Value("env") final String env,
+        @Value("${oauth.google.authorization}") final String authorizationUrl,
+        @Value("${oauth.google.user-access}") final String userAccessUrl,
+        final RestTemplate restTemplate,
+        final ClientRegistrationRepository clientRegistrationRepository
+    ) {
+        this.env = env;
+        this.authorizationUrl = authorizationUrl;
+        this.userAccessUrl = userAccessUrl;
+        this.restTemplate = restTemplate;
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     public GoogleUserInfoResponse getUserInfo(final String code) {
+        if (env.equals("dev")) {
+            return returnMockUserInfo();
+        }
         final GoogleAuthorizationResponse response = getAccessToken(code);
         return getUserInfo(response);
+    }
+
+    private GoogleUserInfoResponse returnMockUserInfo() {
+        final String uuid = UUID.randomUUID().toString();
+        final String name = uuid.substring(1, 9);
+        final String email = name + "@gmail.com";
+        final String imageUrl = "https://shorturl.at/dejs2";
+        return new GoogleUserInfoResponse(uuid, name, email, imageUrl, true);
     }
 
     private GoogleAuthorizationResponse getAccessToken(final String code) {

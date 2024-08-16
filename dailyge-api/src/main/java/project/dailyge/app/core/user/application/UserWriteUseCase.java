e6dc9cacd980c5ed2dac60a18b90f09fbdd6a3d1
devjun10 <devjun1023@gmail.com>
2024-08-16 11:52:59 +0900

    refactor: 중복 사용자 가입 방지 로직 추가 - 중복 사용자 가입을 방지하기 위한 로직 추가

--- dailyge-api/src/main/java/project/dailyge/app/core/user/application/UserWriteUseCase.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/application/UserWriteUseCase.java
@@ -3,11 +3,11 @@ package project.dailyge.app.core.user.application;
 import project.dailyge.entity.user.UserJpaEntity;
 
 public interface UserWriteUseCase {
+    Long save(String email);
+
     UserJpaEntity save(UserJpaEntity user);
 
     UserJpaEntity upsert(UserJpaEntity user);
 
     void delete(Long userId);
-
-    Long getSequence();
 }

b534c5136ac11c455131a2dc2b0e7736eec46e51
JiYoul Choi <beatmejy@naver.com>
2024-08-16 11:36:20 +0900

    [DLG-142] 사용자 삭제 메서드 인자에 final 키워드를 추가한다. (#76)

--- dailyge-api/src/main/java/project/dailyge/app/core/user/persistence/UserWriteDao.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/persistence/UserWriteDao.java
@@ -58,7 +58,7 @@ public class UserWriteDao implements UserEntityWriteRepository {
      */
     @Override
     @Transactional
-    public void delete(UserJpaEntity user) {
+    public void delete(final UserJpaEntity user) {
         entityManager.detach(user);
     }
 }

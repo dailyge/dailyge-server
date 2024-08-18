d5bd3c494d97c5f6888fd7b44922240bf8eb5a71
devjun10 <devjun1023@gmail.com>
2024-08-18 15:06:45 +0900

    refactor: Fixture private 생성자 호출 방지 추가 - Reflection으로 생성자 호출 못 하도록 코드 추가

--- storage/rdb/src/testFixtures/java/project/dailyge/entity/monthly_goal/MonthlyGoalFixture.java
+++ storage/rdb/src/testFixtures/java/project/dailyge/entity/monthly_goal/MonthlyGoalFixture.java
@@ -3,7 +3,7 @@ package project.dailyge.entity.monthly_goal;
 public final class MonthlyGoalFixture {
 
     private MonthlyGoalFixture() {
-
+        throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
     }
 
     public static final MonthlyGoalJpaEntity newMonthlyGoal = new MonthlyGoalJpaEntity(

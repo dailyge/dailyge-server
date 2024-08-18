6e11d27d4ec573c147e013495b358596a650f677
devjun10 <devjun1023@gmail.com>
2024-08-18 17:06:46 +0900

    test: MonthlyGoal 수정 통합 테스트 작성 - 제목/내용 수정 통합 테스트 작성 - 목표 달성 여부 수정 통합 테스트 작성

---
+++ dailyge-api/src/test/java/project/dailyge/app/test/monthly_goal/integrationtest/MonthlyGoalUpdateIntegrationTest.java
@@ -0,0 +1,81 @@
+package project.dailyge.app.test.monthly_goal.integrationtest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import project.dailyge.app.common.DatabaseTestBase;
+import project.dailyge.app.core.monthly_goal.application.MonthlyGoalReadUseCase;
+import project.dailyge.app.core.monthly_goal.application.MonthlyGoalWriteUseCase;
+import project.dailyge.app.core.monthly_goal.application.command.MonthlyGoalCreateCommand;
+import project.dailyge.app.core.monthly_goal.application.command.MonthlyGoalStatusUpdateCommand;
+import project.dailyge.app.core.monthly_goal.application.command.MonthlyGoalUpdateCommand;
+import project.dailyge.app.core.monthly_goal.exception.MonthlyGoalCodeAndMessage;
+import project.dailyge.app.core.monthly_goal.exception.MonthlyGoalTypeException;
+import project.dailyge.entity.monthly_goal.MonthlyGoalJpaEntity;
+
+@DisplayName("[IntegrationTest] 월간 목표 삭제 통합 테스트")
+class MonthlyGoalUpdateIntegrationTest extends DatabaseTestBase {
+
+    @Autowired
+    private MonthlyGoalReadUseCase monthlyGoalReadUseCase;
+
+    @Autowired
+    private MonthlyGoalWriteUseCase monthlyGoalWriteUseCase;
+
+    @Test
+    @DisplayName("MonthlyGoal 상태를 업데이트하면, 상태가 변경된다.")
+    void whenUpdateMonthlyGoalStatusThenStatusShouldBeChanged() {
+        final MonthlyGoalCreateCommand createCommand = new MonthlyGoalCreateCommand("메인 페이지 개발 완료", "서비스 출시.");
+        final Long newMonthlyGoalId = monthlyGoalWriteUseCase.save(dailygeUser, createCommand);
+        final MonthlyGoalStatusUpdateCommand statusUpdateCommand = new MonthlyGoalStatusUpdateCommand(true);
+        monthlyGoalWriteUseCase.update(dailygeUser, newMonthlyGoalId, statusUpdateCommand);
+
+        final MonthlyGoalJpaEntity findMonthlyGoal = monthlyGoalReadUseCase.findById(newMonthlyGoalId);
+        assertTrue(findMonthlyGoal.isDone());
+    }
+
+    @Test
+    @DisplayName("MonthlyGoal 내용을 업데이트하면, 내용이 변경된다.")
+    void whenUpdateMonthlyGoalThenContentsShouldBeChanged() {
+        final MonthlyGoalCreateCommand createCommand = new MonthlyGoalCreateCommand("메인 페이지 개발 완료", "서비스 출시.");
+        final Long newMonthlyGoalId = monthlyGoalWriteUseCase.save(dailygeUser, createCommand);
+        final String changedTitle = "마라톤 참여";
+        final String changedContent = "가족들과 한강 마라톤 참여";
+        final MonthlyGoalUpdateCommand statusUpdateCommand = new MonthlyGoalUpdateCommand(changedTitle, changedContent);
+        monthlyGoalWriteUseCase.update(dailygeUser, newMonthlyGoalId, statusUpdateCommand);
+
+        final MonthlyGoalJpaEntity findMonthlyGoal = monthlyGoalReadUseCase.findById(newMonthlyGoalId);
+        assertAll(
+            () -> assertEquals(changedTitle, findMonthlyGoal.getTitle()),
+            () -> assertEquals(changedContent, findMonthlyGoal.getContent())
+        );
+    }
+
+    @Test
+    @DisplayName("MonthlyGoal 상태를 변경할 때, MonthlyGoal이 존재하지 않으면 MonthlyGoalTypeException이 발생한다.")
+    void whenUpdateMonthlyGoalStatusButNotExistsThenMonthlyGoalTypeExceptionShouldBeHappen() {
+        final Long invalidId = Long.MAX_VALUE;
+        final MonthlyGoalStatusUpdateCommand statusUpdateCommand = new MonthlyGoalStatusUpdateCommand(true);
+
+        assertThatThrownBy(() -> monthlyGoalWriteUseCase.update(dailygeUser, invalidId, statusUpdateCommand))
+            .isInstanceOf(MonthlyGoalTypeException.class)
+            .hasMessage(MonthlyGoalCodeAndMessage.MONTHLY_GOAL_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("MonthlyGoal을 변경할 때, MonthlyGoal이 존재하지 않으면 MonthlyGoalTypeException이 발생한다.")
+    void whenUpdateMonthlyGoalButNotExistsThenMonthlyGoalTypeExceptionShouldBeHappen() {
+        final Long invalidId = Long.MAX_VALUE;
+        final String changedTitle = "마라톤 참여";
+        final String changedContent = "가족들과 한강 마라톤 참여";
+        final MonthlyGoalUpdateCommand statusUpdateCommand = new MonthlyGoalUpdateCommand(changedTitle, changedContent);
+
+        assertThatThrownBy(() -> monthlyGoalWriteUseCase.update(dailygeUser, invalidId, statusUpdateCommand))
+            .isInstanceOf(MonthlyGoalTypeException.class)
+            .hasMessage(MonthlyGoalCodeAndMessage.MONTHLY_GOAL_NOT_FOUND.getMessage());
+    }
+}

5f775bf1351316eb167885c962a22044acfcc1ca
devjun10 <devjun1023@gmail.com>
2024-08-18 15:55:57 +0900

    feat: MonthlyGoal 삭제 API 개발 - 삭제 API 개발

--- dailyge-api/src/main/java/project/dailyge/app/core/monthly_goal/application/service/MonthlyGoalWriteService.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/monthly_goal/application/service/MonthlyGoalWriteService.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import project.dailyge.app.common.annotation.ApplicationLayer;
 import project.dailyge.app.common.auth.DailygeUser;
+import project.dailyge.app.common.exception.UnAuthorizedException;
 import project.dailyge.app.core.monthly_goal.application.MonthlyGoalWriteUseCase;
 import project.dailyge.app.core.monthly_goal.application.command.MonthlyGoalCreateCommand;
+import static project.dailyge.app.core.monthly_goal.exception.MonthlyGoalCodeAndMessage.MONTHLY_GOAL_NOT_FOUND;
+import project.dailyge.app.core.monthly_goal.exception.MonthlyGoalTypeException;
+import project.dailyge.entity.monthly_goal.MonthlyGoalEntityReadRepository;
 import project.dailyge.entity.monthly_goal.MonthlyGoalEntityWriteRepository;
 import project.dailyge.entity.monthly_goal.MonthlyGoalJpaEntity;
 
@@ -13,6 +17,7 @@ import project.dailyge.entity.monthly_goal.MonthlyGoalJpaEntity;
 @RequiredArgsConstructor
 public class MonthlyGoalWriteService implements MonthlyGoalWriteUseCase {
 
+    private final MonthlyGoalEntityReadRepository monthlyGoalReadRepository;
     private final MonthlyGoalEntityWriteRepository monthlyGoalWriteRepository;
 
     @Override
@@ -23,5 +28,19 @@ public class MonthlyGoalWriteService implements MonthlyGoalWriteUseCase {
     ) {
         final MonthlyGoalJpaEntity newMonthlyGoal = command.toEntity(dailygeUser);
         return monthlyGoalWriteRepository.save(newMonthlyGoal);
+    }
+
+    @Override
+    @Transactional
+    public void delete(
+        final DailygeUser dailygeUser,
+        final Long monthlyGoalId
+    ) {
+        final MonthlyGoalJpaEntity findMonthlyGoal = monthlyGoalReadRepository.findById(monthlyGoalId)
+            .orElseThrow(() -> MonthlyGoalTypeException.from(MONTHLY_GOAL_NOT_FOUND));
+        if (!dailygeUser.isValid(findMonthlyGoal.getUserId())) {
+            throw new UnAuthorizedException();
+        }
+        findMonthlyGoal.delete();
     }
 }
---
+++ dailyge-api/src/main/java/project/dailyge/app/core/monthly_goal/exception/MonthlyGoalCodeAndMessage.java
@@ -0,0 +1,31 @@
+package project.dailyge.app.core.monthly_goal.exception;
+
+import lombok.Getter;
+import project.dailyge.app.codeandmessage.CodeAndMessage;
+
+@Getter
+public enum MonthlyGoalCodeAndMessage implements CodeAndMessage {
+    MONTHLY_GOAL_NOT_FOUND(404, "월간 목표를 찾을 수 없습니다."),
+    MONTHLY_GOAL_UN_RESOLVED_EXCEPTION(500, "작업이 실패했습니다. 진행중인 작업을 확인해주세요.");
+
+    private final int code;
+    private final String message;
+
+    MonthlyGoalCodeAndMessage(
+        final int code,
+        final String message
+    ) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}

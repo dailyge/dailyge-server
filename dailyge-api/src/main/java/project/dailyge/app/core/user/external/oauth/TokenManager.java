61db9168618ce5b8dfe731485b04730b6b0ab045
JiYoul Choi <beatmejy@naver.com>
2024-08-20 14:08:15 +0900

    [DLG-166] 로그인 시 User 조회 로직을 수정한다. (#99)
    * refactor: TokenManager 예외 중복 코드 삭제
    
    * refactor: 회원가입 시 User 조회 로직 리팩토링
    
    * refactor: 사용자 ID가 없을 경우 새로 만든 값 반환하도록 수정
    
    * test: 깨진 테스트 재 수정

--- dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/TokenManager.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/TokenManager.java
@@ -23,46 +23,28 @@ public class TokenManager {
         final Long userId,
         final String refreshToken
     ) {
-        try {
-            executeRedisCommand(() -> {
-                final byte[] compressedRefreshToken = compressStringAsByteArray(refreshToken.getBytes(UTF_8));
-                redisTemplate.opsForValue().set(getKey(userId), compressedRefreshToken);
-                return null;
-            });
-        } catch (RedisException ex) {
-            throw new ExternalServerException(ex.getMessage(), BAD_GATEWAY);
-        } catch (Exception ex) {
-            throw new ExternalServerException(ex.getMessage(), INTERNAL_SERVER_ERROR);
-        }
+        executeRedisCommand(() -> {
+            final byte[] compressedRefreshToken = compressStringAsByteArray(refreshToken.getBytes(UTF_8));
+            redisTemplate.opsForValue().set(getKey(userId), compressedRefreshToken);
+            return null;
+        });
     }
 
     public String getRefreshToken(final Long userId) {
-        try {
-            return executeRedisCommand(() -> {
-                final byte[] refreshToken = redisTemplate.opsForValue().get(getKey(userId));
-                if (refreshToken == null) {
-                    return null;
-                }
-                return decompressAsString(refreshToken);
-            });
-        } catch (RedisException ex) {
-            throw new ExternalServerException(ex.getMessage(), BAD_GATEWAY);
-        } catch (Exception ex) {
-            throw new ExternalServerException(ex.getMessage(), INTERNAL_SERVER_ERROR);
-        }
+        return executeRedisCommand(() -> {
+            final byte[] refreshToken = redisTemplate.opsForValue().get(getKey(userId));
+            if (refreshToken == null) {
+                return null;
+            }
+            return decompressAsString(refreshToken);
+        });
     }
 
     public void deleteRefreshToken(final Long userId) {
-        try {
-            executeRedisCommand(() -> {
-                redisTemplate.delete(getKey(userId));
-                return null;
-            });
-        } catch (RedisException ex) {
-            throw new ExternalServerException(ex.getMessage(), BAD_GATEWAY);
-        } catch (Exception ex) {
-            throw new ExternalServerException(ex.getMessage(), INTERNAL_SERVER_ERROR);
-        }
+        executeRedisCommand(() -> {
+            redisTemplate.delete(getKey(userId));
+            return null;
+        });
     }
 
     private String executeRedisCommand(final Supplier<String> command) {

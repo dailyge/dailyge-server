1cd9bbebd4ae094f8d210829647ed46a72060154
devjun10 <devjun1023@gmail.com>
2024-08-26 15:24:22 +0900

    feat: 올바르지 않은 URL을 요청한 경우, 404 응답을 반환하도록 한다. - 올바르지 않은 URL 경로 예외처리 추가

--- dailyge-api/src/main/java/project/dailyge/app/DailygeApplication.java
+++ dailyge-api/src/main/java/project/dailyge/app/DailygeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import project.dailyge.common.configuration.RedisConfig;
 import project.dailyge.configuration.JpaConfig;
 import project.dailyge.configuration.QueryDslConfig;
@@ -12,6 +13,7 @@ import project.dailyge.document.configuration.MongoConfig;
 import project.dailyge.lock.configuration.RedissonConfig;
 
 @EnableAsync
+@EnableWebMvc
 @SpringBootApplication
 @RequiredArgsConstructor
 @Import(value = {JpaConfig.class, MongoConfig.class, RedisConfig.class, RedissonConfig.class, QueryDslConfig.class})

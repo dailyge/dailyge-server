27de8d7ad9562cdd33401df94a71b561768578f7
beatmejy <beatmejy@naver.com>
2024-08-28 11:22:04 +0900

    feat: 어드민 CommonException 추가  - 레디스 사용 시 발생하는 ExternalServerException도 추가

---
+++ admin-api/src/main/java/project/dailyge/app/common/exception/CommonException.java
@@ -0,0 +1,29 @@
+package project.dailyge.app.common.exception;
+
+import lombok.Getter;
+import project.dailyge.app.codeandmessage.CodeAndMessage;
+
+@Getter
+public class CommonException extends RuntimeException {
+
+    private String detailMessage;
+    private final CodeAndMessage codeAndMessage;
+
+    public CommonException(final CodeAndMessage codeAndMessage) {
+        super(codeAndMessage.message());
+        this.codeAndMessage = codeAndMessage;
+    }
+
+    public CommonException(
+        final String detailMessage,
+        final CodeAndMessage codeAndMessage
+    ) {
+        super(detailMessage);
+        this.detailMessage = detailMessage;
+        this.codeAndMessage = codeAndMessage;
+    }
+
+    public int getCode() {
+        return codeAndMessage.code();
+    }
+}

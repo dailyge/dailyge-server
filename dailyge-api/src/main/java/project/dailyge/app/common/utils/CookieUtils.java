f5f2941cc219bf94960bfe8d54a4b874638607e7
beatmejy <beatmejy@naver.com>
2024-08-31 13:23:49 +0900

    style: final 키워드 추가

--- dailyge-api/src/main/java/project/dailyge/app/common/utils/CookieUtils.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/utils/CookieUtils.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.Cookie;
 public class CookieUtils {
 
     public static Cookie clearCookie(final String name) {
-        Cookie cookie = new Cookie(name, null);
+        final Cookie cookie = new Cookie(name, null);
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setDomain(".dailyge.com");

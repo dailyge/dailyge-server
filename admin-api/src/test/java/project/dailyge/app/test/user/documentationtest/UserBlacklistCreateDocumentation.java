8ccfcb7713a383eb14250612260f37272ef66607
kmularise <kmula222@gmail.com>
2024-09-02 17:37:42 +0900

    test: Youl님 요청으로 이한 disabled 처리

--- admin-api/src/test/java/project/dailyge/app/test/user/documentationtest/UserBlacklistCreateDocumentation.java
+++ admin-api/src/test/java/project/dailyge/app/test/user/documentationtest/UserBlacklistCreateDocumentation.java
@@ -3,6 +3,7 @@ package project.dailyge.app.test.user.documentationtest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,11 @@ import project.dailyge.app.user.presentation.request.UserBlacklistCreateRequest;
 import project.dailyge.core.cache.user.UserCache;
 import project.dailyge.core.cache.user.UserCacheWriteUseCase;
 import project.dailyge.entity.user.Role;
+
 import static io.restassured.RestAssured.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document;
-import static project.dailyge.app.test.user.documentationtest.snippet.UserSnippet.USER_BLACKLIST_ACCESS_TOKEN_COOKIE_SNIPPET;
-import static project.dailyge.app.test.user.documentationtest.snippet.UserSnippet.USER_BLACKLIST_CREATE_PATH_PARAMETER_SNIPPET;
-import static project.dailyge.app.test.user.documentationtest.snippet.UserSnippet.USER_BLACKLIST_CREATE_RESPONSE_FIELDS_SNIPPET;
-import static project.dailyge.app.test.user.documentationtest.snippet.UserSnippet.USER_BLACKLIST_CREATE_RESPONSE_SNIPPET;
-import static project.dailyge.app.test.user.documentationtest.snippet.UserSnippet.createIdentifier;
+import static project.dailyge.app.test.user.documentationtest.snippet.UserSnippet.*;
 
 @DisplayName("[DocumentationTest] User Blacklist 등록 문서화 테스트")
 class UserBlacklistCreateDocumentation extends DatabaseTestBase {
@@ -49,6 +47,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[RestDocs] 요청한 사용자를 블랙리스트에 넣는다.")
     void whenSaveUserBlacklistThenResultShouldBe_200_RestDocs() throws JsonProcessingException {
         given(this.specification)
@@ -68,6 +67,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[Swagger] 요청한 사용자를 블랙리스트에 넣는다.")
     void whenSaveUserBlacklistThenResultShouldBe_200_Swagger() throws JsonProcessingException {
         final RestDocumentationFilter filter = UserBlacklistSnippet.createUserBlacklistCreateFilter(createIdentifier("UserBlacklistCreate", 200));
@@ -84,6 +84,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[Swagger] 어드민이 아니라면, 403 UN AUTHORIZED를 반환한다.")
     void whenSaveUserBlacklistThenResultShouldBe_403_Swagger() throws JsonProcessingException {
         final RestDocumentationFilter filter = UserBlacklistSnippet.createUserBlacklistCreateFilter(createIdentifier("UserBlacklistCreate", 403));
@@ -100,6 +101,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[Swagger] 요청한 사용자가 존재하지 않는다면, 404 NOT FOUND를 반환한다.")
     void whenSaveUserBlacklistThenResultShouldBe_404_Swagger() throws JsonProcessingException {
         final RestDocumentationFilter filter = UserBlacklistSnippet.createUserBlacklistCreateFilter(createIdentifier("UserBlacklistCreate", 404));

e985a41e4c97749e2c4847e49f3930a705952a83
devjun10 <>
2024-09-02 14:41:41 +0900

    feat: MonthlyGoal 커서 페이징 Api 개발 - 최초/이후 커서 페이징 조회 로직 구분

---
+++ dailyge-api/src/main/java/project/dailyge/app/common/annotation/CursorPageable.java
@@ -0,0 +1,12 @@
+package project.dailyge.app.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.PARAMETER})
+public @interface CursorPageable {
+    int limit() default 10;
+}

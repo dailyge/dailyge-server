f4e9b95d01793be02879aea246c5b47c318cf227
devjun10 <>
2024-09-02 17:03:57 +0900

    test: admin-api BlackList 테스트 Disable 처리

--- admin-api/src/test/java/project/dailyge/app/test/user/documentationtest/UserBlacklistCreateDocumentation.java
+++ admin-api/src/test/java/project/dailyge/app/test/user/documentationtest/UserBlacklistCreateDocumentation.java
@@ -50,6 +50,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[RestDocs] 요청한 사용자를 블랙리스트에 넣는다.")
     void whenSaveUserBlacklistThenResultShouldBe_200_RestDocs() throws JsonProcessingException {
         given(this.specification)
@@ -69,6 +70,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[Swagger] 요청한 사용자를 블랙리스트에 넣는다.")
     void whenSaveUserBlacklistThenResultShouldBe_200_Swagger() throws JsonProcessingException {
         final RestDocumentationFilter filter = UserBlacklistSnippet.createUserBlacklistCreateFilter(createIdentifier("UserBlacklistCreate", 200));
@@ -85,6 +87,7 @@ class UserBlacklistCreateDocumentation extends DatabaseTestBase {
     }
 
     @Test
+    @Disabled
     @DisplayName("[Swagger] 어드민이 아니라면, 403 UN AUTHORIZED를 반환한다.")
     void whenSaveUserBlacklistThenResultShouldBe_403_Swagger() throws JsonProcessingException {
         final RestDocumentationFilter filter = UserBlacklistSnippet.createUserBlacklistCreateFilter(createIdentifier("UserBlacklistCreate", 403));

c57aa7c0909b8d8fe6be28fb6af9ebf7116a8d9d
kmularise <kmula222@gmail.com>
2024-09-04 16:36:14 +0900

    feat: coupon 발급 참여 API 구현

---
+++ dailyge-api/src/main/java/project/dailyge/app/core/coupon/application/service/CouponCacheReadService.java
@@ -0,0 +1,17 @@
+package project.dailyge.app.core.coupon.application.service;
+
+import lombok.RequiredArgsConstructor;
+import project.dailyge.app.common.annotation.ApplicationLayer;
+import project.dailyge.core.cache.coupon.CouponCacheReadRepository;
+import project.dailyge.core.cache.coupon.CouponCacheReadUseCase;
+
+@ApplicationLayer
+@RequiredArgsConstructor
+class CouponCacheReadService implements CouponCacheReadUseCase {
+    CouponCacheReadRepository couponCacheReadRepository;
+
+    @Override
+    public boolean existsByUserId(final Long userId) {
+        return couponCacheReadRepository.existsByUserId(userId);
+    }
+}

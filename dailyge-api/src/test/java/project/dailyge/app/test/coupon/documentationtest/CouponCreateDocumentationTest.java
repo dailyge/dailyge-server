4142a534e0c741add9a46885ffb15e539d9aa562
devjun10 <>
2024-09-09 03:49:37 +0900

    style: SonarCloud 경고 반영 - 코드 스멜 제거
    [DLG-252] 쿠폰 발급 요청 묶음을 처리하는 API를 개발한다. (#140)

--- dailyge-api/src/test/java/project/dailyge/app/test/coupon/documentationtest/CouponCreateDocumentationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/coupon/documentationtest/CouponCreateDocumentationTest.java
@@ -1,25 +1,19 @@
 package project.dailyge.app.test.coupon.documentationtest;
 
-import static io.restassured.RestAssured.given;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import project.dailyge.app.common.DatabaseTestBase;
 
-import java.time.LocalDateTime;
+import static io.restassured.RestAssured.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @DisplayName("[DocumentationTest] 쿠폰 발급 신청 문서화 테스트")
 class CouponCreateDocumentationTest extends DatabaseTestBase {
 
-    @BeforeEach
-    void setUp() {
-        nowTime = LocalDateTime.now();
-    }
-
     @Test
     @DisplayName("[RestDocs] 쿠폰 발급에 참여하면 201 Created 응답을 받는다.")
-    void whenCreateCouponThenResponseShouldBe_201_RestDocs() {
+    void whenCreateCouponThenResponseShouldBe_201_RestDocs() throws JsonProcessingException {
         given(this.specification)
             .relaxedHTTPSValidation()
             .contentType(APPLICATION_JSON_VALUE)

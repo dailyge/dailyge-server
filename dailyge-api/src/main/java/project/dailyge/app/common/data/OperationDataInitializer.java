9d55aab372d76f8a3e687872c5a02a8e418ce127
devjun10 <>
2024-09-13 16:12:42 +0900

    refactor: 데이터 초기화 설정 변경

--- dailyge-api/src/main/java/project/dailyge/app/common/data/OperationDataInitializer.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/data/OperationDataInitializer.java
@@ -1,107 +1,91 @@
 package project.dailyge.app.common.data;
 
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
-import jakarta.annotation.PreDestroy;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
-import javax.sql.DataSource;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import project.dailyge.entity.user.UserJpaEntity;
+import project.dailyge.core.cache.user.UserCache;
+import static project.dailyge.entity.user.Role.NORMAL;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Slf4j
-// @Component
+@Component
 @Profile({"local", "dev"})
 @RequiredArgsConstructor
 public class OperationDataInitializer implements CommandLineRunner {
 
     private static final String LOCAL = "local";
-
-    @Value("${env}")
-    private String env;
-
-    @Value("${profile.nickname}")
-    private String nickname;
+    private static final String DEV = "dev";
 
     @Value("${profile.email}")
     private String email;
 
-    private final DataSource dataSource;
-    private final JdbcTemplate jdbcTemplate;
+    private final Set<String> tableNames;
     private final EntityManager entityManager;
-    private final Set<String> collectionNames = new HashSet<>();
-    private final MongoTemplate mongoTemplate;
+    private final JdbcTemplate jdbcTemplate;
+    private final OperationDataWriteDao operationWriteDao;
 
     @Override
     @Transactional
     public void run(final String... args) {
-        log.info(env);
-        if (isEnv(LOCAL)) {
-            initSchema();
-            initCollection();
+        if (isValidEnv()) {
+            clearData();
             initData();
+            log.info("Data initialized success.");
         }
     }
 
-    public void initSchema() {
-        if (isEnv(LOCAL)) {
-            final Path rootPath = Paths.get("").toAbsolutePath();
-            final Path sqlFilePath = rootPath.resolve("config/schema/schema.sql");
-            try (Connection connection = dataSource.getConnection()) {
-                ScriptUtils.executeSqlScript(connection, new FileSystemResource(sqlFilePath));
-            } catch (SQLException ex) {
-                throw new RuntimeException("Failed to execute SQL script", ex);
-            }
+    private void clearData() {
+        final Set<String> tableNames = entityManager.getMetamodel()
+            .getEntities().stream()
+            .filter(isEntityType())
+            .map(toLowerCase())
+            .collect(Collectors.toSet());
+        this.tableNames.addAll(tableNames);
+        for (final String table : this.tableNames) {
+            final String query = String.format("TRUNCATE %s", table);
+            jdbcTemplate.execute(query);
         }
     }
 
-    private void initCollection() {
-        final MongoDatabase database = mongoTemplate.getDb();
-        final MongoIterable<String> collectionNames = database.listCollectionNames();
-        for (final String collectionName : collectionNames) {
-            this.collectionNames.add(collectionName);
-        }
+    @NotNull
+    private Predicate<EntityType<?>> isEntityType() {
+        return entityType -> entityType.getJavaType().getAnnotation(Entity.class) != null;
+    }
+
+    @NotNull
+    private Function<EntityType<?>, String> toLowerCase() {
+        return entityType -> entityType.getName().toLowerCase();
+    }
+
+    private boolean isValidEnv() {
+        return isEnv(LOCAL) || isEnv(DEV);
     }
 
     public void initData() {
-        if (isEnv(LOCAL)) {
-            try {
-                entityManager.persist(new UserJpaEntity(1L, nickname, email));
-            } catch (Exception ex) {
-                log.error("Data initialization failed: {}", ex.getMessage());
-            }
-        }
-    }
-
-    @PreDestroy
-    @Transactional
-    public void clearData() {
-        if (isEnv(LOCAL)) {
-            jdbcTemplate.execute("TRUNCATE users");
-            for (final String collectionName : this.collectionNames) {
-                mongoTemplate.remove(new Query(), collectionName);
-            }
+        try {
+            final UserCache userCache = new UserCache(1L, email, email, "", NORMAL.name());
+            operationWriteDao.save(userCache);
+            operationWriteDao.save(email);
+        } catch (Exception ex) {
+            log.error("Data initialization failed: {}", ex.getMessage());
         }
     }
 
     private boolean isEnv(final String env) {
-        return LOCAL.equals(env);
+        return LOCAL.equals(env)
+            || DEV.equals(env);
     }
 }

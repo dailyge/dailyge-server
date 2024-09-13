fc74cba7847cabeb7702e6bfe9390b4c77758a17
devjun10 <>
2024-09-14 05:16:24 +0900

    refactor: LuaScript를 통해 한 번에 캐시, 블랙리스트 검증 - 레디스 호출 횟수 감소를 위해 LuaScript 사용

--- dailyge-api/src/main/java/project/dailyge/app/core/user/persistence/UserCacheReadDao.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/persistence/UserCacheReadDao.java
@@ -2,12 +2,15 @@ package project.dailyge.app.core.user.persistence;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.lettuce.core.RedisException;
+import static java.util.Arrays.asList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Repository;
 import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.BAD_GATEWAY;
 import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.INTERNAL_SERVER_ERROR;
 import project.dailyge.app.common.exception.CommonException;
+import static project.dailyge.app.common.script.LuaScript.USER_CACHE_SEARCH_LUA_SCRIPT;
 import static project.dailyge.common.configuration.CompressionHelper.decompressAsObjWithZstd;
 import project.dailyge.core.cache.user.UserCache;
 import project.dailyge.core.cache.user.UserCacheReadRepository;
@@ -22,11 +25,14 @@ public class UserCacheReadDao implements UserCacheReadRepository {
     @Override
     public UserCache findById(final Long userId) {
         try {
-            final byte[] cache = redisTemplate.opsForValue().get(getKey(userId));
-            if (cache == null) {
+            final DefaultRedisScript<byte[]> script = new DefaultRedisScript<>();
+            script.setScriptText(USER_CACHE_SEARCH_LUA_SCRIPT);
+            script.setResultType(byte[].class);
+            final byte[] findCache = redisTemplate.execute(script, asList(getCacheKey(userId), getBlacklistKey(userId)));
+            if (findCache == null || findCache.length == 0) {
                 return null;
             }
-            return decompressAsObjWithZstd(cache, UserCache.class, objectMapper);
+            return decompressAsObjWithZstd(findCache, UserCache.class, objectMapper);
         } catch (RedisException ex) {
             throw CommonException.from(ex.getMessage(), BAD_GATEWAY);
         } catch (Exception ex) {
@@ -37,7 +43,7 @@ public class UserCacheReadDao implements UserCacheReadRepository {
     @Override
     public boolean existsById(final Long userId) {
         try {
-            final Boolean hasKey = redisTemplate.hasKey(getKey(userId));
+            final Boolean hasKey = redisTemplate.hasKey(getCacheKey(userId));
             return Boolean.TRUE.equals(hasKey);
         } catch (RedisException ex) {
             throw CommonException.from(ex.getMessage(), BAD_GATEWAY);
@@ -46,7 +52,11 @@ public class UserCacheReadDao implements UserCacheReadRepository {
         }
     }
 
-    private static String getKey(Long userId) {
+    private static String getCacheKey(Long userId) {
         return String.format("user:cache:%s", userId);
+    }
+
+    private static String getBlacklistKey(Long userId) {
+        return String.format("user:blacklist:%s", userId);
     }
 }

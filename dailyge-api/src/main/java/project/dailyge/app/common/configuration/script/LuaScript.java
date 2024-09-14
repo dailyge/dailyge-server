2928a57a0904e489af4804b29a17b63697fe3795
devjun10 <>
2024-09-14 22:30:29 +0900

    style: CheckStyle 경고 반영

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/script/LuaScript.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/script/LuaScript.java
@@ -2,17 +2,17 @@ package project.dailyge.app.common.configuration.script;
 
 public interface LuaScript {
     String USER_CACHE_SEARCH_LUA_SCRIPT =
-        "local cache_key = KEYS[1] " +
-            "local blacklist_key = KEYS[2] " +
-            "local cache = redis.call('GET', cache_key) " +
-            "if cache then " +
-            "    local is_blacklisted = redis.call('EXISTS', blacklist_key) " +
-            "    if is_blacklisted == 1 then " +
-            "        return nil " +
-            "    else " +
-            "        return cache " +
-            "    end " +
-            "else " +
-            "    return nil " +
-            "end";
+        "local cache_key = KEYS[1] "
+            + "local blacklist_key = KEYS[2] "
+            + "local cache = redis.call('GET', cache_key) "
+            + "if cache then "
+            + "    local is_blacklisted = redis.call('EXISTS', blacklist_key) "
+            + "    if is_blacklisted == 1 then "
+            + "        return nil "
+            + "    else "
+            + "        return cache "
+            + "    end "
+            + "else "
+            + "    return nil "
+            + "end";
 }

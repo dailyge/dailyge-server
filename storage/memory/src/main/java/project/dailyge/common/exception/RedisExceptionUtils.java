74660293cfe0d837e112948466c6197f36e94f39
devjun10 <>
2024-09-15 09:11:27 +0900

    refactor: 유틸 클래스에 리플렉션 호출 방지 추가 - private 생성자를 호출하지 못하도록 코드 추가

--- storage/memory/src/main/java/project/dailyge/common/exception/RedisExceptionUtils.java
+++ storage/memory/src/main/java/project/dailyge/common/exception/RedisExceptionUtils.java
@@ -2,6 +2,10 @@ package project.dailyge.common.exception;
 
 public final class RedisExceptionUtils {
 
+    private RedisExceptionUtils() {
+        throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
+    }
+
     public static void resolveRedisSystemException(
         final String message,
         final Runnable runnable

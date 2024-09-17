d5ba8b86dcb9934a8e473401540194c6665b4edb
kmularise <kmula222@gmail.com>
2024-09-18 00:06:24 +0900

    test: 문서화 및 통합 테스트 추가

---
+++ dailyge-api/src/test/java/project/dailyge/app/test/coupon/integrationtest/CouponBulkSchedulerTest.java
@@ -0,0 +1,45 @@
+package project.dailyge.app.test.coupon.integrationtest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import project.dailyge.app.common.DatabaseTestBase;
+import project.dailyge.app.core.coupon.application.scheduler.CouponBulkScheduler;
+import project.dailyge.app.core.coupon.persistence.CouponEventParticipant;
+import project.dailyge.app.core.coupon.persistence.CouponInMemoryRepository;
+import project.dailyge.core.cache.coupon.CouponCacheReadUseCase;
+
+import java.util.stream.LongStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("[통합 테스트] CouponBulkScheduler 동작 테스트")
+public class CouponBulkSchedulerTest extends DatabaseTestBase {
+
+    @Autowired
+    private CouponInMemoryRepository couponInMemoryRepository;
+
+    @Autowired
+    private CouponBulkScheduler couponBulkScheduler;
+
+    @Autowired
+    private CouponCacheReadUseCase couponCacheReadUseCase;
+
+    @BeforeEach
+    void setUp() {
+        LongStream.range(1L, 10000L)
+            .forEach(number -> couponInMemoryRepository.save(new CouponEventParticipant(number, number)));
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("스케줄러가 시작하면 폴링 작업을 한다.")
+    void whenStartSchedulerThenPollingRuns() throws InterruptedException {
+        couponBulkScheduler.startFixedTask(5);
+        Thread.sleep(30000);
+        couponBulkScheduler.stop();
+        assertTrue(couponCacheReadUseCase.existsByUserId(1L));
+    }
+}

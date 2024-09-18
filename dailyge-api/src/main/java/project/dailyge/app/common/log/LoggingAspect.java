6a25aaedd9a27a707768a73fa7eb0d93a3cc7550
Jun <92818747+devjun10@users.noreply.github.com>
2024-09-19 06:22:51 +0900

    [DLG-317] 불필요한 Import를 제거한다. (#173)
    Co-authored-by: devjun10 <>

--- dailyge-api/src/main/java/project/dailyge/app/common/log/LoggingAspect.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/log/LoggingAspect.java
@@ -1,17 +1,15 @@
 package project.dailyge.app.common.log;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static java.lang.String.format;
-import static java.time.LocalDateTime.now;
 import static java.time.temporal.ChronoUnit.MILLIS;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.MDC;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 import project.dailyge.app.common.annotation.ApplicationLayer;
 import project.dailyge.app.common.annotation.ExternalLayer;
 import project.dailyge.app.common.annotation.FacadeLayer;
@@ -30,8 +28,6 @@ import java.lang.annotation.Annotation;
 import java.time.LocalDateTime;
 
 @Slf4j
-@Aspect
-@Component
 @Profile("!test")
 @RequiredArgsConstructor
 public class LoggingAspect {
@@ -83,10 +79,10 @@ public class LoggingAspect {
         final ProceedingJoinPoint joinPoint,
         final String layer
     ) throws Throwable {
-        final LocalDateTime startTime = now();
+        final LocalDateTime startTime = LocalDateTime.now();
         logStart(joinPoint, format("%s%s", layer, IN_COMING), startTime);
         final Object result = joinPoint.proceed();
-        final LocalDateTime endTime = now();
+        final LocalDateTime endTime = LocalDateTime.now();
         logEnd(joinPoint, format("%s%s", layer, OUT_GOING), startTime, endTime, result);
         return result;
     }
@@ -95,23 +91,19 @@ public class LoggingAspect {
         final ProceedingJoinPoint joinPoint,
         final String layer,
         final LocalDateTime startTime
-    ) {
-        try {
-            final String traceId = MDC.get(TRACE_ID);
-            final String ip = MDC.get(IP);
-            final String method = MDC.get(METHOD);
-            final String path = MDC.get(PATH);
-            final String visitor = MDC.get(DAILYGE_USER_KEY);
-            final Object[] argsArray = joinPoint.getArgs();
-            final String args = (argsArray != null) ? objectMapper.writeValueAsString(argsArray) : EMPTY_ARRAY;
-            increaseOrder();
-            final int order = Integer.parseInt(MDC.get(LOG_ORDER));
-            log.info(
-                createLogMessage(order, layer, path, method, traceId, ip, startTime, 0, args, null, visitor, INFO)
-            );
-        } catch (Exception ex) {
-            log.error("Logging failed");
-        }
+    ) throws JsonProcessingException {
+        final String traceId = MDC.get(TRACE_ID);
+        final String ip = MDC.get(IP);
+        final String method = MDC.get(METHOD);
+        final String path = MDC.get(PATH);
+        final String visitor = MDC.get(DAILYGE_USER_KEY);
+        final Object[] argsArray = joinPoint.getArgs();
+        final String args = (argsArray != null) ? objectMapper.writeValueAsString(argsArray) : EMPTY_ARRAY;
+        increaseOrder();
+        final int order = Integer.parseInt(MDC.get(LOG_ORDER));
+        log.info(
+            createLogMessage(order, layer, path, method, traceId, ip, startTime, 0, args, null, visitor, INFO)
+        );
     }
 
     private void logEnd(
@@ -120,34 +112,26 @@ public class LoggingAspect {
         final LocalDateTime startTime,
         final LocalDateTime endTime,
         final Object result
-    ) {
-        try {
-            final String traceId = MDC.get(TRACE_ID);
-            final String ip = MDC.get(IP);
-            final String method = MDC.get(METHOD);
-            final String path = MDC.get(PATH);
-            final String visitor = MDC.get(DAILYGE_USER_KEY);
-            final Object[] argsArray = joinPoint.getArgs();
-            final String args = (argsArray != null) ? objectMapper.writeValueAsString(argsArray) : EMPTY_ARRAY;
-            final long duration = MILLIS.between(startTime, endTime);
-            increaseOrder();
-            final int order = Integer.parseInt(MDC.get(LOG_ORDER));
-            log.info(
-                createLogMessage(order, layer, path, method, traceId, ip, endTime, duration, args, result, visitor, INFO)
-            );
-        } catch (Exception ex) {
-            log.error("Logging failed");
-        }
+    ) throws JsonProcessingException {
+        final String traceId = MDC.get(TRACE_ID);
+        final String ip = MDC.get(IP);
+        final String method = MDC.get(METHOD);
+        final String path = MDC.get(PATH);
+        final String visitor = MDC.get(DAILYGE_USER_KEY);
+        final Object[] argsArray = joinPoint.getArgs();
+        final String args = (argsArray != null) ? objectMapper.writeValueAsString(argsArray) : EMPTY_ARRAY;
+        final long duration = MILLIS.between(startTime, endTime);
+        increaseOrder();
+        final int order = Integer.parseInt(MDC.get(LOG_ORDER));
+        log.info(
+            createLogMessage(order, layer, path, method, traceId, ip, endTime, duration, args, result, visitor, INFO)
+        );
     }
 
     private void increaseOrder() {
-        try {
-            final String logOrder = MDC.get(LOG_ORDER);
-            final int order = logOrder != null ? Integer.parseInt(logOrder) : 0;
-            MDC.put(LOG_ORDER, String.valueOf(order + 1));
-        } catch (Exception ex) {
-            log.error("Ordering failed");
-        }
+        final String logOrder = MDC.get(LOG_ORDER);
+        final int order = logOrder != null ? Integer.parseInt(logOrder) : 0;
+        MDC.put(LOG_ORDER, String.valueOf(order + 1));
     }
 
     private <T extends Annotation> T getAnnotation(
--- dailyge-api/src/main/java/project/dailyge/app/common/log/MdcFilter.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/log/MdcFilter.java
@@ -7,12 +7,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.time.temporal.ChronoUnit.MILLIS;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import project.dailyge.app.common.auth.DailygeUser;
 import static project.dailyge.app.common.auth.DailygeUser.getDailygeUser;
 import static project.dailyge.app.common.utils.IpUtils.extractIpAddress;
@@ -32,14 +33,28 @@ import static project.dailyge.document.common.UuidGenerator.createTimeBasedUUID;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Base64;
 
 @Slf4j
 @Order(1)
-@Component
 @Profile("!test")
 public class MdcFilter implements Filter {
 
+    private static final int USERNAME = 0;
+    private static final int PASSWORD = 1;
+    private static final String DELIMITER = ":";
     private static final String DAILYGE_USER = "dailyge-user";
+
+    private final String username;
+    private final String password;
+
+    public MdcFilter(
+        @Value("${basic-auth.username}") final String username,
+        @Value("${basic-auth.password}") final String password
+    ) {
+        this.username = username;
+        this.password = password;
+    }
 
     @Override
     public void doFilter(
@@ -54,41 +69,16 @@ public class MdcFilter implements Filter {
         final String method = request.getMethod();
         final LocalDateTime startTime = LocalDateTime.now();
         try {
-            writeIncomingLog(traceId, userIp, method, path, startTime);
-            filterChain.doFilter(servletRequest, servletResponse);
-        } finally {
-            writeOutgoingLog(servletRequest, startTime, path, method, traceId, userIp);
-        }
-    }
-
-    private static void writeIncomingLog(
-        final String traceId,
-        final String userIp,
-        final String method,
-        final String path,
-        final LocalDateTime startTime
-    ) {
-        try {
             MDC.put(TRACE_ID, traceId);
             MDC.put(IP, userIp);
             MDC.put(LOG_ORDER, LOG_ORDER_INI);
             MDC.put(METHOD, method);
             MDC.put(PATH, path);
-            log.info(createIncomingLog(path, method, traceId, userIp, startTime));
-        } catch (Exception ex) {
-            log.error("Incoming logging failed.");
-        }
-    }
 
-    private static void writeOutgoingLog(
-        final ServletRequest servletRequest,
-        final LocalDateTime startTime,
-        final String path,
-        final String method,
-        final String traceId,
-        final String userIp
-    ) {
-        try {
+            final String longMessage = createIncomingLog(path, method, traceId, userIp, startTime);
+            log.info(longMessage);
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
             final LocalDateTime endTime = LocalDateTime.now();
             final long duration = MILLIS.between(startTime, endTime);
 
@@ -97,8 +87,6 @@ public class MdcFilter implements Filter {
             servletRequest.removeAttribute(DAILYGE_USER);
             log.info(longMessage);
             MDC.clear();
-        } catch (Exception ex) {
-            log.error("Outgoing logging failed.");
         }
     }
 
@@ -155,5 +143,32 @@ public class MdcFilter implements Filter {
         final int next = order + 1;
         MDC.put(LOG_ORDER, String.valueOf(next));
         return next;
+    }
+
+    public boolean isValidRequest(final ServletRequest servletRequest) {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final String authorization = request.getHeader("x-dailyge-auth");
+        if (authorization == null || !authorization.startsWith("Basic ")) {
+            return false;
+        }
+
+        try {
+            final String base64Credentials = authorization.substring("Basic".length()).trim();
+            final byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+            final String decodedCredentials = new String(decodedBytes, UTF_8);
+            final String[] credentials = decodedCredentials.split(DELIMITER, 2);
+            final String username = credentials[USERNAME].trim();
+            final String password = credentials[PASSWORD].trim();
+            return isValidUser(username, password);
+        } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException ex) {
+            return false;
+        }
+    }
+
+    private boolean isValidUser(
+        final String username,
+        final String password
+    ) {
+        return this.username.equals(username) && this.password.equals(password);
     }
 }

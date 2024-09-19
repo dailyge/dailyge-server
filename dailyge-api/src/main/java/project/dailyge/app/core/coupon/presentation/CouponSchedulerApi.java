1245f9ad00d0e6f80a5b98b394fa79861636a3f1
kmularise <kmula222@gmail.com>
2024-09-19 16:45:10 +0900

    refactor: 코드 컨벤션 준수

--- dailyge-api/src/main/java/project/dailyge/app/core/coupon/presentation/CouponSchedulerApi.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/coupon/presentation/CouponSchedulerApi.java
@@ -1,18 +1,18 @@
 package project.dailyge.app.core.coupon.presentation;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.dailyge.app.common.annotation.PresentationLayer;
+import project.dailyge.app.common.response.ApiResponse;
 import project.dailyge.app.core.coupon.application.scheduler.CouponBulkScheduler;
 import project.dailyge.app.core.coupon.presentation.request.ScheduleRateRequest;
 import project.dailyge.core.cache.coupon.CouponCacheWriteUseCase;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.CREATED;
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.NO_CONTENT;
 
 @PresentationLayer
 @RequestMapping("/api")
@@ -22,18 +22,14 @@ public class CouponSchedulerApi {
     private final CouponCacheWriteUseCase couponCacheWriteUseCase;
 
     @PostMapping(path = "/coupons/scheduling")
-    public ResponseEntity<Void> startScheduler(@RequestBody final ScheduleRateRequest schedulerRateRequest) {
+    public ApiResponse<Void> startScheduler(@RequestBody final ScheduleRateRequest schedulerRateRequest) {
         couponBulkScheduler.startFixedTask(schedulerRateRequest.period(), couponCacheWriteUseCase::saveBulks);
-        return ResponseEntity
-            .status(CREATED)
-            .build();
+        return ApiResponse.from(CREATED);
     }
 
     @DeleteMapping(path = "/coupons/scheduling")
-    public ResponseEntity<Void> stopScheduler() {
+    public ApiResponse<Void> stopScheduler() {
         couponBulkScheduler.stop();
-        return ResponseEntity
-            .status(NO_CONTENT)
-            .build();
+        return ApiResponse.from(NO_CONTENT);
     }
 }

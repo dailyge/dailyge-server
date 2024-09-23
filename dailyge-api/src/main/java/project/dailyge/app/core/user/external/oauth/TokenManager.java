962912302ba1cc81a262d3b8de19f66e9cc94555
Yuijin Kim(yuikim) <106499310+kmularise@users.noreply.github.com>
2024-09-23 18:59:18 +0900

    [DLG-328] 부하테스트를 위해 refresh token String으로 저장한다. (#183)

--- dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/TokenManager.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/external/oauth/TokenManager.java
@@ -1,42 +1,35 @@
 package project.dailyge.app.core.user.external.oauth;
 
 import io.lettuce.core.RedisException;
-import static java.nio.charset.StandardCharsets.UTF_8;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.BAD_GATEWAY;
-import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.INTERNAL_SERVER_ERROR;
 import project.dailyge.app.common.annotation.ExternalLayer;
 import project.dailyge.app.common.exception.CommonException;
-import static project.dailyge.common.configuration.CompressionHelper.compressStringAsByteArray;
-import static project.dailyge.common.configuration.CompressionHelper.decompressAsString;
 
 import java.util.function.Supplier;
+
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.BAD_GATEWAY;
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
 @ExternalLayer(value = "TokenManager")
 public class TokenManager {
 
-    private final RedisTemplate<String, byte[]> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void saveRefreshToken(
         final Long userId,
         final String refreshToken
     ) {
         executeRedisCommand(() -> {
-            final byte[] compressedRefreshToken = compressStringAsByteArray(refreshToken.getBytes(UTF_8));
-            redisTemplate.opsForValue().set(getKey(userId), compressedRefreshToken);
+            redisTemplate.opsForValue().set(getKey(userId), refreshToken);
             return null;
         });
     }
 
     public String getRefreshToken(final Long userId) {
         return executeRedisCommand(() -> {
-            final byte[] refreshToken = redisTemplate.opsForValue().get(getKey(userId));
-            if (refreshToken == null) {
-                return null;
-            }
-            return decompressAsString(refreshToken);
+            return redisTemplate.opsForValue().get(getKey(userId));
         });
     }
 

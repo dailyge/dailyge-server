4ac3bbd16935223a1b8af739b85c806a3781aa4e
kmularise <kmula222@gmail.com>
2024-09-30 15:06:11 +0900

    fix: Layer 어노테이션 value() 추가

--- admin-api/src/main/java/project/dailyge/app/common/annotation/ApplicationLayer.java
+++ admin-api/src/main/java/project/dailyge/app/common/annotation/ApplicationLayer.java
@@ -1,10 +1,12 @@
 package project.dailyge.app.common.annotation;
 
+import org.springframework.stereotype.Service;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import org.springframework.stereotype.Service;
 
 @Service
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ApplicationLayer {
+    String value() default "";
 }

86a064c43c080a8c134829449686e96b600bba9e
kmularise <kmula222@gmail.com>
2024-09-30 15:20:12 +0900

    test: 쿠폰 발급 과정 추가 테스트 - 당첨자 선정 테스트 수정 - 쿠폰 발급 과정 테스트 추가

--- admin-api/src/test/java/project/dailyge/app/test/coupon/integrationtest/CouponUseCaseIntegrationTest.java
+++ admin-api/src/test/java/project/dailyge/app/test/coupon/integrationtest/CouponUseCaseIntegrationTest.java
@@ -14,6 +14,8 @@ import project.dailyge.core.cache.coupon.CouponEventWriteRepository;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("[IntegrationTest] 당첨자 선정 통합 테스트 ")
 class CouponUseCaseIntegrationTest extends DatabaseTestBase {
 
@@ -41,11 +43,13 @@ class CouponUseCaseIntegrationTest extends DatabaseTestBase {
         final int queueCount = 10;
         final int winnerCount = 1000;
         final List<CouponEvent> couponWinners = CouponWinnerFixture.makeExpectedCouponEvents(winnerCount);
+        final List<Long> expectedUserIds = couponWinners.stream().map(CouponEvent::getUserId).toList();
         final List<List<CouponEvent>> candidates = CouponWinnerFixture.makeRandomData(totalCount, queueCount, couponWinners);
         for (List<CouponEvent> couponEvents : candidates) {
             couponEventWriteRepository.saveBulks(couponEvents, 1L);
         }
-        couponUseCase.pickWinners(1000, 1L);
-        //TODO: 당첨자에 대해 쿠폰 발급 추가 시 검증
+        final List<Long> actualUserIds = couponUseCase.pickWinners(1000, 1L);
+        actualUserIds.sort(Long::compare);
+        assertEquals(expectedUserIds, actualUserIds);
     }
 }

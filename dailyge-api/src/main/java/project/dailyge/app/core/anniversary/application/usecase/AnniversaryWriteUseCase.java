c3a67b1784edaad4689c1fe987a5432183d2cee9
devjun10 <>
2024-10-04 17:43:51 +0900

    feat: Anniversary 삭제 API를 개발한다. - 기념일 삭제 API 개발 - Soft Delete

--- dailyge-api/src/main/java/project/dailyge/app/core/anniversary/application/usecase/AnniversaryWriteUseCase.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/anniversary/application/usecase/AnniversaryWriteUseCase.java
@@ -2,10 +2,13 @@ package project.dailyge.app.core.anniversary.application.usecase;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.UN_AUTHORIZED;
 import project.dailyge.app.common.annotation.ApplicationLayer;
 import project.dailyge.app.common.auth.DailygeUser;
+import project.dailyge.app.common.exception.CommonException;
 import project.dailyge.app.core.anniversary.application.AnniversaryWriteService;
 import project.dailyge.app.core.anniversary.application.command.AnniversaryCreateCommand;
+import static project.dailyge.app.core.anniversary.exception.AnniversaryCodeAndMessage.ANNIVERSARY_NOT_FOUND;
 import static project.dailyge.app.core.anniversary.exception.AnniversaryCodeAndMessage.DUPLICATED_ANNIVERSARY;
 import project.dailyge.app.core.anniversary.exception.AnniversaryTypeException;
 import project.dailyge.entity.anniversary.AnniversaryEntityReadRepository;
@@ -30,5 +33,19 @@ class AnniversaryWriteUseCase implements AnniversaryWriteService {
         }
         final AnniversaryJpaEntity newAnniversary = command.toEntity(dailygeUser);
         return anniversaryEntityWriteRepository.save(newAnniversary);
+    }
+
+    @Override
+    @Transactional
+    public void delete(
+        final DailygeUser dailygeUser,
+        final Long anniversaryId
+    ) {
+        final AnniversaryJpaEntity findAnniversary = anniversaryEntityReadRepository.findById(anniversaryId)
+            .orElseThrow(() -> AnniversaryTypeException.from(ANNIVERSARY_NOT_FOUND));
+        if (!dailygeUser.isValid(findAnniversary.getUserId())) {
+            throw CommonException.from(UN_AUTHORIZED);
+        }
+        findAnniversary.delete();
     }
 }

bc0aea7d1badb8c717beb6ef1444c0ee4e220fba
devjun10 <>
2024-10-04 13:43:34 +0900

    feat: Emoji 엔티티 매핑

---
+++ storage/rdb/src/main/java/project/dailyge/entity/emoji/EmojiJpaEntity.java
@@ -0,0 +1,23 @@
+package project.dailyge.entity.emoji;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.dailyge.entity.BaseEntity;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "emojis")
+public class EmojiJpaEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "emoji")
+    private String emoji;
+}

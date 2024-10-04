6e6604ac6bd5b5accc72ba20d5323b746d8d425b
devjun10 <>
2024-10-04 14:17:07 +0900

    feat: exists 메서드 추가 - 빠진 커밋 추가

--- storage/rdb/src/main/java/project/dailyge/entity/anniversary/AnniversaryEntityReadRepository.java
+++ storage/rdb/src/main/java/project/dailyge/entity/anniversary/AnniversaryEntityReadRepository.java
@@ -1,7 +1,10 @@
 package project.dailyge.entity.anniversary;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface AnniversaryEntityReadRepository {
     Optional<AnniversaryJpaEntity> findById(Long anniversaryId);
+
+    boolean exists(String name, LocalDateTime date);
 }

c67b1271c3290c139db668d8756eb5633c6ea082
beatmejy <beatmejy@naver.com>
2024-10-05 19:38:31 +0900

    refactor: 개행 수정

--- dailyge-api/src/main/java/project/dailyge/app/core/retrospect/application/RetrospectReadService.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/retrospect/application/RetrospectReadService.java
@@ -3,6 +3,5 @@ package project.dailyge.app.core.retrospect.application;
 import project.dailyge.entity.retrospect.RetrospectJpaEntity;
 
 public interface RetrospectReadService {
-
     RetrospectJpaEntity findById(Long retrospectId);
 }
--- dailyge-api/src/main/java/project/dailyge/app/core/retrospect/application/RetrospectWriteService.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/retrospect/application/RetrospectWriteService.java
@@ -4,6 +4,5 @@ import project.dailyge.app.common.auth.DailygeUser;
 import project.dailyge.app.core.retrospect.application.command.RetrospectCreateCommand;
 
 public interface RetrospectWriteService {
-
     Long save(DailygeUser dailygeUser, RetrospectCreateCommand command);
 }

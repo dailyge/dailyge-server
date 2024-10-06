a1a0bde2a3fab45a760a475511f299f19e6369c2
devjun10 <>
2024-10-06 16:40:16 +0900

    feat: 정적 문서 설정 추가 - RestDocs를 사용하기 위한 설정 추가

--- dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/WebConfiguration.java
+++ dailyge-api/src/main/java/project/dailyge/app/common/configuration/web/WebConfiguration.java
@@ -1,14 +1,16 @@
 package project.dailyge.app.common.configuration.web;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.dailyge.app.common.auth.TokenProvider;
+import project.dailyge.app.core.common.web.AuthArgumentResolver;
 import project.dailyge.app.core.common.web.CouponApplyInterceptor;
 import project.dailyge.app.core.common.web.CursorPagingArgumentResolver;
 import project.dailyge.app.core.common.web.LoginInterceptor;
@@ -17,17 +19,31 @@ import project.dailyge.core.cache.user.UserCacheReadUseCase;
 import java.util.List;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfiguration implements WebMvcConfigurer {
 
+    private final String env;
     private final UserCacheReadUseCase userCacheReadUseCase;
     private final TokenProvider tokenProvider;
     private final LoginInterceptor loginInterceptor;
     private final CouponApplyInterceptor couponApplyInterceptor;
 
+    public WebConfiguration(
+        @Value("${env}") final String env,
+        final UserCacheReadUseCase userCacheReadUseCase,
+        final TokenProvider tokenProvider,
+        final LoginInterceptor loginInterceptor,
+        final CouponApplyInterceptor couponApplyInterceptor
+    ) {
+        this.env = env;
+        this.userCacheReadUseCase = userCacheReadUseCase;
+        this.tokenProvider = tokenProvider;
+        this.loginInterceptor = loginInterceptor;
+        this.couponApplyInterceptor = couponApplyInterceptor;
+    }
+
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthArgumentResolver(userCacheReadUseCase, tokenProvider));
+        resolvers.add(new AuthArgumentResolver(env, userCacheReadUseCase, tokenProvider));
         resolvers.add(new CursorPagingArgumentResolver());
     }
 
@@ -45,5 +61,11 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Bean
     public RequestContextListener requestContextListener() {
         return new RequestContextListener();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+            .addResourceLocations("classpath:/static/docs/");
     }
 }

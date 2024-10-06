155d1ca8b9621a6e4b9f61e7eea3a396d95e8747
devjun10 <>
2024-10-06 11:06:50 +0900

    refactor: 공휴일 조회 일자 기준 변경 - 서비스를 시작한 날짜로 부터 4년 전인 2020년 부터 조회

--- dailyge-api/src/main/java/project/dailyge/app/core/holiday/presentation/validator/HolidayClientValidator.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/holiday/presentation/validator/HolidayClientValidator.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class HolidayClientValidator {
 
     public void validateYear(final int year) {
-        if (year <= 0 || year >= 2100) {
+        if (year <= 2020 || year >= 2100) {
             throw new IllegalArgumentException("올바른 년도를 입력해주세요.");
         }
     }

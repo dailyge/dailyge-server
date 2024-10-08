6e0af1c2686b4956453ec2218c8406cc0f4f23c6
kmularise <kmula222@gmail.com>
2024-10-08 17:09:43 +0900

    test: 주간 목표 통합테스트 패키지 이동

--- dailyge-api/src/test/java/project/dailyge/app/test/weeklygoal/integrationtest/WeeklyGoalCreateIntegrationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/weeklygoal/integrationtest/WeeklyGoalCreateIntegrationTest.java
@@ -1,4 +1,4 @@
-package project.dailyge.app.test.weeklygoal;
+package project.dailyge.app.test.weeklygoal.integrationtest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;

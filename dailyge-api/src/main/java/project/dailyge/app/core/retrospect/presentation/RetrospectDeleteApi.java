f5196cfef1776b543888ccf3d52be23d38f56583
beatmejy <beatmejy@naver.com>
2024-10-08 18:42:11 +0900

    style: 병합 후 깨진 import 수정

--- dailyge-api/src/main/java/project/dailyge/app/core/retrospect/presentation/RetrospectDeleteApi.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/retrospect/presentation/RetrospectDeleteApi.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.dailyge.app.common.annotation.LoginUser;
 import project.dailyge.app.common.annotation.PresentationLayer;
-import project.dailyge.app.common.auth.DailygeUser;
-import project.dailyge.app.common.auth.LoginUser;
 import project.dailyge.app.common.response.ApiResponse;
+import project.dailyge.app.core.common.auth.DailygeUser;
 import project.dailyge.app.core.retrospect.application.RetrospectWriteService;
 import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.NO_CONTENT;
 
--- dailyge-api/src/main/java/project/dailyge/app/core/weeklygoal/application/usecase/WeeklyGoalWriteUseCase.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/weeklygoal/application/usecase/WeeklyGoalWriteUseCase.java
@@ -3,7 +3,7 @@ package project.dailyge.app.core.weeklygoal.application.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import project.dailyge.app.common.annotation.ApplicationLayer;
-import project.dailyge.app.common.auth.DailygeUser;
+import project.dailyge.app.core.common.auth.DailygeUser;
 import project.dailyge.app.core.weeklygoal.application.WeeklyGoalWriteService;
 import project.dailyge.app.core.weeklygoal.application.command.WeeklyGoalCreateCommand;
 import project.dailyge.entity.weeklygoal.WeeklyGoalEntityWriteRepository;

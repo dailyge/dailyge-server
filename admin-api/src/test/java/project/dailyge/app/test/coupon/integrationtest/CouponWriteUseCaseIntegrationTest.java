9f6100c40da49ef110728dd7c096135b97c7f868
devjun10 <>
2024-10-08 18:10:56 +0900

    test: 깨진 당첨자 테스트 복구

--- admin-api/src/test/java/project/dailyge/app/test/coupon/integrationtest/CouponWriteUseCaseIntegrationTest.java
+++ admin-api/src/test/java/project/dailyge/app/test/coupon/integrationtest/CouponWriteUseCaseIntegrationTest.java
@@ -38,18 +38,16 @@ class CouponWriteUseCaseIntegrationTest extends DatabaseTestBase {
     @Test
     @DisplayName("당첨자 선정작업을 시작하면 당첨자를 적절하게 선정해야 한다.")
     void whenExecuteWinnerSelectionThenResultShouldBeCorrect() {
-        final int totalCount = 10000;
+        final int totalCount = 10_000;
         final int queueCount = 10;
-        final int winnerCount = 1000;
+        final int winnerCount = 1_000;
         final List<CouponEvent> couponWinners = CouponWinnerFixture.makeExpectedCouponEvents(winnerCount);
         final List<Long> expectedUserIds = couponWinners.stream().map(CouponEvent::getUserId).toList();
         final List<List<CouponEvent>> candidates = CouponWinnerFixture.makeRandomData(totalCount, queueCount, couponWinners);
         for (List<CouponEvent> couponEvents : candidates) {
             couponEventWriteRepository.saveBulks(couponEvents, 1L);
         }
-        couponEventWriteService.pickWinners(1000, 1L);
-        //TODO: 당첨자에 대해 쿠폰 발급 추가 시 검증
-        final List<Long> actualUserIds = couponEventWriteService.pickWinners(1000, 1L);
+        final List<Long> actualUserIds = couponEventWriteService.pickWinners(1_000, 1L);
         actualUserIds.sort(Long::compare);
         assertEquals(expectedUserIds, actualUserIds);
     }

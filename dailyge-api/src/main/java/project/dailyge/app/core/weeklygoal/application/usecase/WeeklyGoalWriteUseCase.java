7ba4a037cc9aade8f9069723a2f0894d9d8174f5
kmularise <kmula222@gmail.com>
2024-10-09 15:10:58 +0900

    feat: 주간 목표 수정 API 개발

--- dailyge-api/src/main/java/project/dailyge/app/core/weeklygoal/application/usecase/WeeklyGoalWriteUseCase.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/weeklygoal/application/usecase/WeeklyGoalWriteUseCase.java
@@ -3,17 +3,25 @@ package project.dailyge.app.core.weeklygoal.application.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import project.dailyge.app.common.annotation.ApplicationLayer;
+import project.dailyge.app.common.exception.CommonException;
 import project.dailyge.app.core.common.auth.DailygeUser;
 import project.dailyge.app.core.weeklygoal.application.WeeklyGoalWriteService;
 import project.dailyge.app.core.weeklygoal.application.command.WeeklyGoalCreateCommand;
+import project.dailyge.app.core.weeklygoal.application.command.WeeklyGoalUpdateCommand;
+import project.dailyge.app.core.weeklygoal.exception.WeeklyGoalTypeException;
+import project.dailyge.entity.weeklygoal.WeeklyGoalEntityReadRepository;
 import project.dailyge.entity.weeklygoal.WeeklyGoalEntityWriteRepository;
 import project.dailyge.entity.weeklygoal.WeeklyGoalJpaEntity;
+
+import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.INVALID_USER_ID;
+import static project.dailyge.app.core.weeklygoal.exception.WeeklyGoalCodeAndMessage.WEEKLY_GOAL_NOT_FOUND;
 
 @RequiredArgsConstructor
 @ApplicationLayer(value = "WeeklyGoalWriteUseCase")
 class WeeklyGoalWriteUseCase implements WeeklyGoalWriteService {
 
-    private final WeeklyGoalEntityWriteRepository weeklyGoalEntityWriteRepository;
+    private final WeeklyGoalEntityWriteRepository weeklyGoalWriteRepository;
+    private final WeeklyGoalEntityReadRepository weeklyGoalReadRepository;
 
     @Override
     @Transactional
@@ -22,6 +30,36 @@ class WeeklyGoalWriteUseCase implements WeeklyGoalWriteService {
         final WeeklyGoalCreateCommand command
     ) {
         final WeeklyGoalJpaEntity newWeeklyGoal = command.toEntity(dailygeUser);
-        return weeklyGoalEntityWriteRepository.save(newWeeklyGoal);
+        return weeklyGoalWriteRepository.save(newWeeklyGoal);
+    }
+
+    @Override
+    @Transactional
+    public void update(
+        final DailygeUser dailygeUser,
+        final Long weeklyGoalId,
+        final WeeklyGoalUpdateCommand command
+    ) {
+        final WeeklyGoalJpaEntity findWeeklyGoal = weeklyGoalReadRepository.findById(weeklyGoalId)
+            .orElseThrow(() -> WeeklyGoalTypeException.from(WEEKLY_GOAL_NOT_FOUND));
+        if (!dailygeUser.isValid(findWeeklyGoal.getUserId())) {
+            throw CommonException.from(INVALID_USER_ID);
+        }
+        findWeeklyGoal.update(command.title(), command.content());
+    }
+
+    @Override
+    @Transactional
+    public void update(
+        final DailygeUser dailygeUser,
+        final Long weeklyGoalId,
+        final Boolean done
+    ) {
+        final WeeklyGoalJpaEntity findWeeklyGoal = weeklyGoalReadRepository.findById(weeklyGoalId)
+            .orElseThrow(() -> WeeklyGoalTypeException.from(WEEKLY_GOAL_NOT_FOUND));
+        if (!dailygeUser.isValid(findWeeklyGoal.getUserId())) {
+            throw CommonException.from(INVALID_USER_ID);
+        }
+        findWeeklyGoal.updateDone(done);
     }
 }

84880b932a2d35c1a8c91e299bebf82d5809200c
beatmejy <beatmejy@naver.com>
2024-10-18 14:46:01 +0900

    test: 테스트 코드 수정

--- dailyge-api/src/test/java/project/dailyge/app/test/retrospect/integrationtest/RetrospectReadIntegrationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/retrospect/integrationtest/RetrospectReadIntegrationTest.java
@@ -11,6 +11,7 @@ import project.dailyge.app.core.retrospect.application.RetrospectReadService;
 import project.dailyge.app.core.retrospect.application.RetrospectWriteService;
 import project.dailyge.app.core.retrospect.application.command.RetrospectCreateCommand;
 import project.dailyge.app.page.CustomPageable;
+import project.dailyge.app.response.AsyncPagingResponse;
 import project.dailyge.entity.retrospect.RetrospectJpaEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -37,41 +38,46 @@ class RetrospectReadIntegrationTest extends DatabaseTestBase {
     @DisplayName("회고 조회 시 정상적으로 반환한다.")
     void whenFindRetrospectThenResultShouldBeCorrectly() {
         final CustomPageable page = CustomPageable.createPage(1, 10);
-        final List<RetrospectJpaEntity> findRetrospects = retrospectReadService.findRetrospectByPage(dailygeUser, page);
+        final AsyncPagingResponse<RetrospectJpaEntity> asyncPagingResponse = retrospectReadService.findRetrospectAndTotalCountByPage(dailygeUser, page);
+        final List<RetrospectJpaEntity> findRetrospects = asyncPagingResponse.data();
+        final int totalCount = asyncPagingResponse.totalCount();
 
         assertEquals(10, findRetrospects.size());
+        assertEquals(10, totalCount);
     }
 
     @Test
     @DisplayName("회고 조회 시 페이징을 최신 데이터 순으로 반환한다.")
     void whenFindRetrospectThenResultShouldBeRecentDatePagingReturned() {
         final CustomPageable page = CustomPageable.createPage(3, 1);
-        final List<RetrospectJpaEntity> findRetrospects = retrospectReadService.findRetrospectByPage(dailygeUser, page);
+        final AsyncPagingResponse<RetrospectJpaEntity> asyncPagingResponse = retrospectReadService.findRetrospectAndTotalCountByPage(dailygeUser, page);
 
+        final List<RetrospectJpaEntity> findRetrospects = asyncPagingResponse.data();
         assertEquals(1, findRetrospects.size());
 
         final RetrospectJpaEntity findRetrospect = findRetrospects.get(0);
         assertEquals(8, findRetrospect.getId());
     }
 
-
     @Test
     @DisplayName("페이지 이후의 데이터가 없다면, 빈 리스트을 반환한다.")
     void whenNoDataAfterThePageThenResultShouldBeEmptyList() {
         final CustomPageable page = CustomPageable.createPage(2, 10);
-        final List<RetrospectJpaEntity> findRetrospects = retrospectReadService.findRetrospectByPage(dailygeUser, page);
+        final AsyncPagingResponse<RetrospectJpaEntity> asyncPagingResponse = retrospectReadService.findRetrospectAndTotalCountByPage(dailygeUser, page);
+        final List<RetrospectJpaEntity> findRetrospects = asyncPagingResponse.data();
 
         assertTrue(findRetrospects.isEmpty());
     }
 
-
     @Test
     @DisplayName("자신의 회고 전체 개수만 반환한다.")
     void whenFindTotalCountThenResultShouldBeMyTotalRetrospectCount() {
+        final CustomPageable page = CustomPageable.createPage(1, 10);
         final RetrospectCreateCommand createCommand = new RetrospectCreateCommand("회고 제목", "회고 내용", now.atTime(0, 0, 0, 0), false);
         retrospectWriteService.save(invalidUser, createCommand);
 
-        final long totalCount = retrospectReadService.findTotalCount(dailygeUser);
+        final AsyncPagingResponse<RetrospectJpaEntity> asyncPagingResponse = retrospectReadService.findRetrospectAndTotalCountByPage(dailygeUser, page);
+        final int totalCount = asyncPagingResponse.totalCount();
 
         assertEquals(10, totalCount);
     }

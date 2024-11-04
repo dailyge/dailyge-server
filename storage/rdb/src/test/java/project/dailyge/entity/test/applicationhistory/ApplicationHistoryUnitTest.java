b62704167282022b78753a7c0d56464b3445f78b
JiYoul Choi <beatmejy@naver.com>
2024-11-04 16:27:07 +0900

    [DLG-372] 지원 이력 테이블을 설계한다. (#223)

---
+++ storage/rdb/src/test/java/project/dailyge/entity/test/applicationhistory/ApplicationHistoryUnitTest.java
@@ -0,0 +1,31 @@
+package project.dailyge.entity.test.applicationhistory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import project.dailyge.entity.applicationhistory.ApplicationHistoryFixture;
+import project.dailyge.entity.applicationhistory.ApplicationHistoryJpaEntity;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static project.dailyge.entity.applicationhistory.ApplicationStatus.BEFORE_SUBMISSION;
+
+@DisplayName("[UnitTest] ApplicationHistory 단위 테스트")
+class ApplicationHistoryUnitTest {
+
+    @Test
+    @DisplayName("올바른 인자가 들어오면 제출 이력이 생성된다.")
+    void whenValidArgumentsProvidedThenApplicationHistoryIsCreated() {
+        final ApplicationHistoryJpaEntity applicationHistory = ApplicationHistoryFixture.newResumeSubmissionHistory;
+
+        assertAll(
+            () -> assertEquals("Dailyge 프론트엔드 개발자 채용 공고", applicationHistory.getTitle()),
+            () -> assertEquals("https://recruit.dailyge.com", applicationHistory.getLink()),
+            () -> assertEquals(BEFORE_SUBMISSION, applicationHistory.getStatus()),
+            () -> assertEquals(LocalDate.of(2024, 11, 4), applicationHistory.getSubmittedDate()),
+            () -> assertEquals(LocalDateTime.of(2024, 11, 4, 0, 0, 0), applicationHistory.getDeadline()),
+            () -> assertEquals(1L, applicationHistory.getCompanyId()),
+            () -> assertEquals(1L, applicationHistory.getUserId())
+        );
+    }
+}
---
+++ storage/rdb/src/testFixtures/java/project/dailyge/entity/applicationhistory/ApplicationHistoryFixture.java
@@ -0,0 +1,24 @@
+package project.dailyge.entity.applicationhistory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import static project.dailyge.entity.applicationhistory.ApplicationStatus.BEFORE_SUBMISSION;
+
+public final class ApplicationHistoryFixture {
+
+    private ApplicationHistoryFixture() {
+        throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
+    }
+
+    public static final ApplicationHistoryJpaEntity newResumeSubmissionHistory = new ApplicationHistoryJpaEntity(
+        "Dailyge 프론트엔드 개발자 채용 공고",
+        "https://recruit.dailyge.com",
+        BEFORE_SUBMISSION,
+        LocalDate.of(2024, 11, 4),
+        LocalDateTime.of(2024,11,4,0,0,0),
+        2024,
+        11,
+        1L,
+        1L
+    );
+}

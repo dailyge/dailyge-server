2ca693861a05b5dc869983a4ab3e5afa1298d005
Jun <92818747+devjun10@users.noreply.github.com>
2024-11-07 05:57:42 +0900

    test: Note 문서화 테스트 작성 - Note 수신/발신 문서화 테스트 작성 - RestDocs/Swagger 케이스

--- dailyge-api/src/test/java/project/dailyge/app/test/note/documentationtest/snippet/NoteSnippet.kt
+++ dailyge-api/src/test/java/project/dailyge/app/test/note/documentationtest/snippet/NoteSnippet.kt
@@ -38,8 +38,31 @@ interface NoteSnippet {
             fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
         )
 
+        val SENT_NOTE_DETAIL_READ_RESPONSE_FIELDS: List<FieldDescriptor> = listOf(
+            fieldWithPath("data.noteId").type(JsonFieldType.NUMBER).description("Note ID"),
+            fieldWithPath("data.title").type(JsonFieldType.STRING).description("쪽지 제목"),
+            fieldWithPath("data.content").type(JsonFieldType.STRING).description("쪽지 내용"),
+            fieldWithPath("data.read").type(JsonFieldType.BOOLEAN).description("읽음 유무"),
+            fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+            fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
+        )
+
+        val RECEIVED_NOTE_DETAIL_READ_RESPONSE_FIELDS: List<FieldDescriptor> = listOf(
+            fieldWithPath("data.noteId").type(JsonFieldType.NUMBER).description("Note ID"),
+            fieldWithPath("data.title").type(JsonFieldType.STRING).description("쪽지 제목"),
+            fieldWithPath("data.content").type(JsonFieldType.STRING).description("쪽지 내용"),
+            fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+            fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
+        )
+
         val NOTE_ID_PATH_VARIABLES_SNIPPET: PathParametersSnippet = pathParameters(NOTE_ID_PATH_PARAMETER_DESCRIPTORS)
         val NOTE_CREATE_REQUEST_SNIPPET: RequestFieldsSnippet = requestFields(NOTE_CREATE_REQUEST_FIELDS)
         val NOTE_CREATE_RESPONSE_SNIPPET: ResponseFieldsSnippet = responseFields(NOTE_CREATE_RESPONSE_FIELDS)
+        val SENT_NOTE_DETAIL_READ_RESPONSE_SNIPPET: ResponseFieldsSnippet = responseFields(
+            SENT_NOTE_DETAIL_READ_RESPONSE_FIELDS
+        )
+        val RECEIVED_NOTE_DETAIL_READ_RESPONSE_FIELDS_RESPONSE_SNIPPET: ResponseFieldsSnippet = responseFields(
+            RECEIVED_NOTE_DETAIL_READ_RESPONSE_FIELDS
+        )
     }
 }

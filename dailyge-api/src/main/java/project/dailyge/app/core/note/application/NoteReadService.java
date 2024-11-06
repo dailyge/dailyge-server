0e10aa4db6b4ac42a140e314880b2988935d8241
Jun <92818747+devjun10@users.noreply.github.com>
2024-11-06 12:29:23 +0900

    [DLG-373] 쪽지 삭제 API를 개발한다. (#224)
    Signed-off-by: Jun <92818747+devjun10@users.noreply.github.com>
    Co-authored-by: JiYoul Choi <beatmejy@naver.com>

--- dailyge-api/src/main/java/project/dailyge/app/core/note/application/NoteReadService.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/note/application/NoteReadService.java
@@ -10,5 +10,9 @@ public interface NoteReadService {
 
     NoteJpaEntity findSentNoteById(DailygeUser dailygeUser, Long noteId);
 
+    NoteJpaEntity findReceivedNoteById(Long userId, Long noteId);
+
+    NoteJpaEntity findSentNoteById(Long userId, Long noteId);
+
     List<NoteJpaEntity> findAll();
 }

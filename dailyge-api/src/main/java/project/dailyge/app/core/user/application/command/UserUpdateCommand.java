e7cc34a1aa092e9c9d7ee5c06f5a9d2f19a9f9af
Jun <92818747+devjun10@users.noreply.github.com>
2024-11-07 12:46:21 +0900

    style: PR 반영 - 개행 추가 - 메서드 개행 추가

--- dailyge-api/src/main/java/project/dailyge/app/core/user/application/command/UserUpdateCommand.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/user/application/command/UserUpdateCommand.java
@@ -1,6 +1,7 @@
 package project.dailyge.app.core.user.application.command;
 
 public record UserUpdateCommand(String nickname) {
+
     @Override
     public String toString() {
         return String.format("{\"nickname\":\"%s\"}", nickname);

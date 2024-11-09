7aa8377aae259f8bf9b993108b30d3653021015a
beatmejy <beatmejy@naver.com>
2024-11-09 17:36:50 +0900

    test: 사용자 로그인, 캐시 테스트 보강

--- dailyge-api/src/test/java/project/dailyge/app/test/user/integrationtest/UserCacheWriteIntegrationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/user/integrationtest/UserCacheWriteIntegrationTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static project.dailyge.document.common.UuidGenerator.createTimeBasedUUID;
 import static project.dailyge.entity.common.EventType.CREATE;
+import static project.dailyge.entity.common.EventType.UPDATE;
 
 @DisplayName("[IntegrationTest] 사용자 캐시 저장 통합 테스트(with Blacklist)")
 class UserCacheWriteIntegrationTest extends DatabaseTestBase {
@@ -34,6 +35,25 @@ class UserCacheWriteIntegrationTest extends DatabaseTestBase {
         final UserJpaEntity saveUser = userWriteService.save(newUser);
 
         final UserEvent userEvent = UserEvent.createEvent(saveUser.getId(), createTimeBasedUUID(), CREATE);
+        userFacade.saveCache(userEvent);
+
+        final UserCache userCache = userCacheReadService.findById(saveUser.getId());
+
+        assertAll(
+            () -> assertEquals(saveUser.getId(), userCache.getId()),
+            () -> assertEquals(saveUser.getEmail(), userCache.getEmail()),
+            () -> assertEquals(saveUser.getNickname(), userCache.getNickname()),
+            () -> assertEquals(saveUser.getRoleAsString(), userCache.getRole())
+        );
+    }
+
+    @Test
+    @DisplayName("사용자 캐시를 UPDATE로 갱신할 경우, Cache가 비어있을 때만 갱신한다.")
+    void whenCacheIsEmptyInUpdateThenCacheShouldBeSaved() {
+        final UserJpaEntity newUser = new UserJpaEntity(null, "blacklistUser", "blacklistUser@gmail.com", "");
+        final UserJpaEntity saveUser = userWriteService.save(newUser);
+
+        final UserEvent userEvent = UserEvent.createEvent(saveUser.getId(), createTimeBasedUUID(), UPDATE);
         userFacade.saveCache(userEvent);
 
         final UserCache userCache = userCacheReadService.findById(saveUser.getId());
--- dailyge-api/src/test/java/project/dailyge/app/test/user/integrationtest/UserCreateIntegrationTest.java
+++ dailyge-api/src/test/java/project/dailyge/app/test/user/integrationtest/UserCreateIntegrationTest.java
@@ -1,8 +1,5 @@
 package project.dailyge.app.test.user.integrationtest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +7,13 @@ import project.dailyge.app.common.DatabaseTestBase;
 import project.dailyge.app.core.user.application.UserReadService;
 import project.dailyge.app.core.user.application.UserWriteService;
 import project.dailyge.app.core.user.exception.UserTypeException;
-import project.dailyge.app.core.user.external.response.GoogleUserInfoResponse;
 import project.dailyge.app.core.user.facade.UserFacade;
 import project.dailyge.entity.user.UserJpaEntity;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.http.HttpStatus.BAD_GATEWAY;
 import static project.dailyge.app.core.user.exception.UserCodeAndMessage.DUPLICATED_EMAIL;
+import static project.dailyge.app.core.user.exception.UserCodeAndMessage.USER_SERVICE_UNAVAILABLE;
 import static project.dailyge.app.test.user.fixture.UserFixture.createUser;
 
 @DisplayName("[IntegrationTest] 사용자 저장 통합 테스트")
@@ -34,11 +27,6 @@ class UserCreateIntegrationTest extends DatabaseTestBase {
 
     @Autowired
     private UserFacade userFacade;
-
-    @BeforeEach
-    void setUp() {
-        WireMock.reset();
-    }
 
     @Test
     @DisplayName("사용자 등록이 정상적으로 성공하면, 사용자 ID는 NULL이 아니다.")
@@ -59,19 +47,35 @@ class UserCreateIntegrationTest extends DatabaseTestBase {
 
     @Test
     @DisplayName("최초 로그인 시, 사용자가 회원가입된다.")
-    void whenFirstLoginThenUserIsShouldBeRegister() throws JsonProcessingException {
-        final String name = "dailyge";
-        final String email = "dailyge@gmail.com";
-        final GoogleUserInfoResponse response = new GoogleUserInfoResponse(name, name, email, "https://shorturl.at/dejs2", true);
-
-        stubFor(get(urlEqualTo("/userinfo"))
-            .willReturn(aResponse()
-                .withStatus(BAD_GATEWAY.value())
-                .withStatusMessage(BAD_GATEWAY.getReasonPhrase())
-                .withBody(objectMapper.writeValueAsString(response))));
-
-        userFacade.login("AuthenticationCode");
+    void whenFirstLoginThenUserIsShouldBeRegister() {
+        final String name = "dailyges";
+        final String email = "dailyges@gmail.com";
+        userFacade.login(name);
 
         assertNotNull(userReadService.findUserIdByEmail(email));
+    }
+
+    @Test
+    @DisplayName("로그인 시 블랙리스트일 경우, UserServiceUnAvailableException이 발생한다.")
+    void whenBlacklistUserLoginThenUserServiceUnAvailableExceptionShouldBeHappen() {
+        final String name = "blacklistUser";
+        final String email = "blacklistUser@gmail.com";
+        final UserJpaEntity blacklistUser = new UserJpaEntity(null, name, email, true);
+        userWriteService.save(blacklistUser);
+
+        assertThatThrownBy(() -> userFacade.login(name))
+            .isExactlyInstanceOf(UserTypeException.from(USER_SERVICE_UNAVAILABLE).getClass())
+            .hasMessage(USER_SERVICE_UNAVAILABLE.message());
+    }
+
+    @Test
+    @DisplayName("정상 사용자가 로그인 시, 예외가 발생하지 않는다.")
+    void whenLoginNormalUserThenShouldNotThrow() {
+        final String name = "dailyges";
+        final String email = "dailyges@gmail.com";
+        final UserJpaEntity registeredUser = new UserJpaEntity(null, name, email, false);
+        userWriteService.save(registeredUser);
+
+        assertDoesNotThrow(() -> userFacade.login(name));
     }
 }

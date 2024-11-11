c6e6d07b3cbc1f26670b685f484337a9eaf1775e
Jun <92818747+devjun10@users.noreply.github.com>
2024-11-11 16:42:43 +0900

    refactor: totalCount 필드 추가 - 전체 갯수를 알 수 있도록 totalCount 개수 추가

--- modules/api/src/main/kotlin/project/dailyge/api/CursorPagingResponse.kt
+++ modules/api/src/main/kotlin/project/dailyge/api/CursorPagingResponse.kt
@@ -5,5 +5,6 @@ class CursorPagingResponse<T>(
     val size: Int,
 ) {
     val data: List<T> = if (_data.size > size) _data.take(size - 1) else _data
+    val totalCount = data.size
     val hasNext: Boolean = _data.size > size
 }

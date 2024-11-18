9fee1ff1ebee138c039514b199421f5a445a0feb
kmularise <kmula222@gmail.com>
2024-11-18 12:57:05 +0900

    feat: 반복 일정 삭제 API 계층 빈 등록

--- dailyge-api/src/main/java/project/dailyge/app/core/task/presentation/TaskRecurrenceDeleteApi.java
+++ dailyge-api/src/main/java/project/dailyge/app/core/task/presentation/TaskRecurrenceDeleteApi.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.dailyge.app.common.annotation.LoginUser;
+import project.dailyge.app.common.annotation.PresentationLayer;
 import project.dailyge.app.common.response.ApiResponse;
 import project.dailyge.app.core.common.auth.DailygeUser;
 import project.dailyge.app.core.task.application.TaskRecurrenceWriteService;
@@ -11,6 +12,7 @@ import project.dailyge.app.core.task.application.TaskRecurrenceWriteService;
 import static project.dailyge.app.codeandmessage.CommonCodeAndMessage.NO_CONTENT;
 
 @RequestMapping(path = {"/api/task-recurrences"})
+@PresentationLayer(value = "TaskRecurrenceDeleteApi")
 public class TaskRecurrenceDeleteApi {
 
     private final TaskRecurrenceWriteService taskRecurrenceWriteService;
